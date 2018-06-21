52947d22851e5910c7b2dd1474ec99f1ce2de41f
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-21 15:53:10 +0200

    examples/memefactory: Update mapping to the generated types

--- examples/memefactory/mapping.ts
+++ examples/memefactory/mapping.ts
@@ -1,12 +1,7 @@
 /// <reference path="./node_modules/assemblyscript/std/assembly.d.ts" />
 /// <reference path="./node_modules/the-graph-wasm/index.d.ts" />
-
-// This class is auto-generated at compile time
-declare class MemeContract {
-  constructor(address: Address)
-  loadRegistryEntry(): Array<Value>
-  loadMeme(): Array<Value>
-}
+/// <reference path="./types/Meme.types.ts" />
+/// <reference path="./types/MemeRegistry.types.ts" />
 
 export function handleRegistryEntryEvent(
   ctx: EventHandlerContext,
@@ -17,28 +12,19 @@ export function handleRegistryEntryEvent(
   let eventType = event.params[1].value.toString()
 
   if (eventType === 'constructed') {
-    let memeContract = new MemeContract(registryEntryAddress)
-    let registryEntryData = memeContract.loadRegistryEntry().pop()
-    let memeData = memeContract.loadMeme().pop()
+    // Create an instance of the 'Meme' contract
+    let memeContract = new Meme(registryEntryAddress)
 
-    if (registryEntryData === null) {
-      throw new Error("Failed to handle 'constructed' event: No entry data found")
-    } else if (memeData === null) {
-      throw new Error("Failed to handle 'constructed' event: No meme data found")
-    } else {
-      let version = Value.fromString(
-        registryEntryData
-          .toMap()
-          .get('version')
-          .toString()
-      )
+    // Obtain registry entry and meme data from the contract
+    let registryEntryData = memeContract.loadRegistryEntry()
+    let memeData = memeContract.loadMeme()
 
-      let meme = new Entity()
-      meme.set('regEntry_address', Value.fromAddress(registryEntryAddress))
-      meme.set('regEntry_version', version)
+    // Create an entity to push into the database
+    let meme = new Entity()
+    meme.set('regEntry_address', Value.fromAddress(registryEntryAddress))
+    meme.set('regEntry_version', Value.fromU256(registryEntryData.value0))
 
-      ctx.db.add('Meme', meme)
-    }
+    ctx.db.add('Meme', meme)
   } else if (eventType === 'challengeCreated') {
     return
   } else if (eventType === 'voteCommitted') {
