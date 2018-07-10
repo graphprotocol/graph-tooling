b7da52513d1c97f3c072262479f5200361e535c1
Jannis Pohlmann <contact@jannispohlmann.de>
2018-07-10 12:48:51 +0200

    examples: Update generated types (now with event types)

--- examples/example-event-handler/types/ExampleDataSet/ExampleContract.types.ts
+++ examples/example-event-handler/types/ExampleDataSet/ExampleContract.types.ts
@@ -1,3 +1,9 @@
+class ExampleEvent extends EthereumEvent {
+  get exampleParam(): string {
+    return this.params[0].value.toString();
+  }
+}
+
 class ExampleContract extends SmartContract {
   static bind(address: Address, blockHash: H256): ExampleContract {
     return new ExampleContract("ExampleContract", address, blockHash);
