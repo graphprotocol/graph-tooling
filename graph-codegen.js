b9a3d72bbe7f1b9e3161aaf677349a7c8263e7dc
Ford Nickels <ford@thegraph.com>
2018-08-03 11:11:13 -0700

    build, codegen: Watch directory for file changes/additions

--- graph-codegen.js
+++ graph-codegen.js
@@ -2,6 +2,7 @@
 
 let app = require('commander')
 let path = require('path')
+let chokidar = require('chokidar');
 
 let TypeGenerator = require('./src/cli/type-generator')
 
@@ -13,6 +14,9 @@ app
     'Output directory for the generated code',
     path.join(process.cwd(), 'dist')
   )
+  .option(
+        '-w, --watch',
+        'Setup directory watching to rebuild when files change')
   .parse(process.argv)
 
 // Obtain the subgraph manifest file
@@ -22,7 +26,44 @@ if (file === null || file === undefined) {
 }
 
 let generator = new TypeGenerator({
-  subgraphManifest: file,
-  outputDir: app.outputDir,
+    subgraphManifest: file,
+    outputDir: app.outputDir,
 })
 generator.generateTypes()
+
+// Watch working directory for file updates or additions, trigger type generation (if watch argument specified)
+if (app.watch) {
+    console.log("Watching files, new types will be generated when file updates or additions are detected")
+    // Initialize watchers
+    let watcher = chokidar.watch('.', {
+        persistent: true,
+        ignoreInitial: true,
+        ignored: [
+            './mappings',
+            './*.ts',
+            /(^|[\/\\])\../,
+            './node_modules',
+            './examples',
+            './dist',
+            '.yarn.lock'
+        ],
+        depth: 3,
+    })
+
+    // Add event listeners.
+    watcher
+        .on('ready', function() {
+            generator.generateTypes()
+            watcher
+                .on('add', path => {
+                    generator.generateTypes()
+                })
+                .on('change', path => {
+                    generator.generateTypes()
+                });
+        })
+} else {
+    generator.generateTypes()
+}
+
+
