61f5d507491546ffef87bc844d9f119bf766ae91
Ford Nickels <ford@thegraph.com>
2018-08-06 14:18:33 -0700

    build, codegen: Exit watcher on SIGINT, longer wait for atomic changes

--- graph-build.js
+++ graph-build.js
@@ -4,9 +4,9 @@ let app = require('commander')
 let path = require('path')
 let ipfsAPI = require('ipfs-api')
 let chokidar = require('chokidar');
-let Compiler = require('./src/cli/compiler')
+let chalk = require('chalk')
 
-const chalk = require('chalk')
+let Compiler = require('./src/cli/compiler')
 
 app
   .version('0.1.0')
@@ -23,7 +23,7 @@ app
   )
   .option('-t, --output-format [format]', 'Output format (wasm, wast)', 'wasm')
   .option('i, --ipfs [addr]', 'IPFS node to use for uploading files')
-  .option('-w, --watch', 'Setup directory watching to rebuild when files change')
+  .option('-w, --watch', 'Rebuild automatically when files change')
 
 app.on('--help', function() {
   console.log('')
@@ -48,50 +48,57 @@ if (file === null || file === undefined) {
 // Connect to the IPFS node (if a node address was provided)
 let ipfs = app.ipfs ? ipfsAPI(app.ipfs) : undefined
 
-let subgraph_generator = new Compiler({
-    ipfs: ipfs,
-    subgraphManifest: file,
-    outputDir: app.outputDir,
-    outputFormat: app.outputFormat,
-    verbosity: app.verbosity,
+let compiler = new Compiler({
+  ipfs: ipfs,
+  subgraphManifest: file,
+  outputDir: app.outputDir,
+  outputFormat: app.outputFormat,
+  verbosity: app.verbosity,
 })
 
 // Watch working directory for file updates or additions, trigger compile (if watch argument specified)
 if (app.watch) {
-    subgraph_generator.logger.info('')
-    subgraph_generator.logger.info(chalk.grey("Watching files, a new build will be triggered when file updates or additions are detected"))
-    // Initialize watchers
-    let watcher = chokidar.watch('.', {
-        persistent: true,
-        ignoreInitial: true,
-        ignored: [
-            /(^|[\/\\])\../,
-            './node_modules',
-            './examples',
-            './dist',
-            '.yarn.lock'
-        ],
-        depth: 3,
-    })
+  compiler.logger.info('')
+  compiler.logger.info('%s %s', chalk.grey("Watching:"), process.cwd())
 
-    // Add event listeners.
-    watcher
-        .on('ready', function() {
-            subgraph_generator.compile()
-            watcher
-                .on('add', path => {
-                    subgraph_generator.logger.info(chalk.grey('New file detected, rebuilding subgraph'))
-                    subgraph_generator.compile()
-                    subgraph_generator.logger.info('')
-                })
-                .on('change', path => {
-                    subgraph_generator.logger.info(chalk.grey('File change detected, rebuilding subgraph'))
-                    subgraph_generator.compile()
-                    subgraph_generator.logger.info('')
-            });
-            subgraph_generator.logger.info('')
-    })
+  // Initialize watcher
+  let watcher = chokidar.watch('.', {
+    persistent: true,
+    ignoreInitial: true,
+    ignored: [
+      './dist',
+      './examples',
+      './node_modules',
+      /(^|[\/\\])\../
+    ],
+    depth: 3,
+    atomic: 500
+  })
+
+  // Add event listeners.
+  watcher
+    .on('ready', function() {
+      compiler.compile()
+      watcher
+        .on('add', path => {
+          compiler.logger.info(chalk.grey('New file detected, rebuilding subgraph'))
+          compiler.compile()
+          compiler.logger.info('')
+        })
+        .on('change', path => {
+          compiler.logger.info(chalk.grey('File change detected, rebuilding subgraph'))
+          compiler.compile()
+          compiler.logger.info('')
+      });
+      compiler.logger.info('')
+  })
+
+  // Catch keyboard interrupt: close watcher and exit process
+  process.on('SIGINT', function() {
+    watcher.close()
+    process.exit()
+  })
 } else {
-    subgraph_generator.compile()
+  compiler.compile()
 }
 
--- graph-codegen.js
+++ graph-codegen.js
@@ -3,9 +3,9 @@
 let app = require('commander')
 let path = require('path')
 let chokidar = require('chokidar');
+let chalk = require('chalk')
 
 let TypeGenerator = require('./src/cli/type-generator')
-const chalk = require('chalk')
 
 app
   .version('0.1.0')
@@ -21,8 +21,8 @@ app
     process.env.LOG_LEVEL || 'info'
   )
   .option(
-        '-w, --watch',
-        'Setup directory watching to rebuild when files change')
+    '-w, --watch',
+    'Rebuild automatically when files change')
   .parse(process.argv)
 
 // Obtain the subgraph manifest file
@@ -32,52 +32,57 @@ if (file === null || file === undefined) {
 }
 
 let generator = new TypeGenerator({
-    subgraphManifest: file,
-    outputDir: app.outputDir,
-    verbosity: app.verbosity
+  subgraphManifest: file,
+  outputDir: app.outputDir,
+  verbosity: app.verbosity
 })
 
 // Watch working directory for file updates or additions, trigger type generation (if watch argument specified)
 if (app.watch) {
-    generator.logger.info('')
-    generator.logger.info(chalk.grey("Watching files, new types will be generated when file updates or additions are detected"))
-    // Initialize watchers
-    let watcher = chokidar.watch('.', {
-        persistent: true,
-        ignoreInitial: true,
-        ignored: [
-            './mappings',
-            './*.ts',
-            /(^|[\/\\])\../,
-            './node_modules',
-            './examples',
-            './dist',
-            '.yarn.lock'
-        ],
-        depth: 3,
-    })
+  generator.logger.info('')
+  generator.logger.info('%s %s', chalk.grey("Watching:"), process.cwd())
+  // Initialize watchers
+  let watcher = chokidar.watch('.', {
+    persistent: true,
+    ignoreInitial: true,
+    ignored: [
+      './**/*.types.ts',
+      './**/*.ts',
+      './dist',
+      './examples',
+      './mappings',
+      './node_modules',
+      /(^|[\/\\])\../
+    ],
+    depth: 3,
+    atomic: 500
+  })
 
-    // Add event listeners.
-    watcher
-        .on('ready', function() {
-            generator.generateTypes()
-            generator.logger.info('')
-            watcher
-                .on('add', path => {
-                    generator.logger.info(chalk.grey('New file detected, rebuilding types'))
-                    generator.generateTypes()
-                    generator.logger.info('')
-                    // generator.logger.info(chalk.green('Types generated'))
-                })
-                .on('change', path => {
-                    generator.logger.info(chalk.grey('File change detected, rebuilding types'))
-                    generator.generateTypes()
-                    generator.logger.info('')
-                    // generator.logger.info(chalk.green('Types generated'))
-                });
+  // Add event listeners.
+  watcher
+    .on('ready', function() {
+      generator.generateTypes()
+      generator.logger.info('')
+      watcher
+        .on('add', path => {
+          generator.logger.info(chalk.grey('New file detected, rebuilding types'))
+          generator.generateTypes()
+          generator.logger.info('')
         })
+        .on('change', path => {
+          generator.logger.info(chalk.grey('File change detected, rebuilding types'))
+          generator.generateTypes()
+          generator.logger.info('')
+        });
+      })
+
+  // Catch keyboard interrupt: close watcher and exit process
+  process.on('SIGINT', function() {
+    watcher.close()
+    process.exit()
+  })
 } else {
-    generator.generateTypes()
+  generator.generateTypes()
 }
 
 
