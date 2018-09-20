8ff90fabb2de03174a5fa1b60e0b80b661541586
Jannis Pohlmann <contact@jannispohlmann.de>
2018-09-20 15:13:26 +0200

    cli: Log error and help text when executed with an unknown command

--- graph.js
+++ graph.js
@@ -5,8 +5,15 @@ var app = require('commander')
 app
   .version('0.1.0')
   .command('codegen [file]', 'Generates TypeScript types for a subgraph')
-  .command('build [file]', 'Compiles a subgraph and uploads it to IPFS', {
-    isDefault: true,
-  })
+  .command('build [file]', 'Compiles a subgraph and uploads it to IPFS')
   .command('deploy [file]', 'Deploys the subgraph to a graph node')
-  .parse(process.argv)
+
+app.command('*', { noHelp: true }).action(cmd => {
+  console.error(' ')
+  console.error('  Invalid command: %s\n', cmd)
+  console.error('  ---')
+  app.outputHelp()
+  process.exit(1)
+})
+
+app.parse(process.argv)
