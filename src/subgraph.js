7bde909cb4f140ccef7b29b3b1e9714a8e07d7c0
Jannis Pohlmann <contact@jannispohlmann.de>
2018-10-09 19:41:14 -0700

    subgraph: Validate subgraphs when loading them

--- src/subgraph.js
+++ src/subgraph.js
@@ -1,14 +1,45 @@
 let fs = require('fs-extra')
 let immutable = require('immutable')
+let path = require('path')
 let yaml = require('js-yaml')
+let graphql = require('graphql/language')
+//let validate = require('@graphprotocol/graphql-data-validation')
+let validation = require('./validation')
 
 module.exports = class Subgraph {
-  static load(path) {
-    let data = yaml.safeLoad(fs.readFileSync(path, 'utf-8'))
+  static validate(data) {
+    // Parse the default subgraph schema
+    let schema = graphql.parse(
+      fs.readFileSync(path.join(__dirname, '..', 'manifest-schema.graphql'), 'utf-8')
+    )
+
+    // Obtain the root `SubgraphManifest` type from the schema
+    let rootType = schema.definitions.find(definition => {
+      return definition.name.value === 'SubgraphManifest'
+    })
+
+    // Validate the subgraph definition using this schema
+    let errors = validation.validateManifest(data, rootType, schema)
+    if (errors.length > 0) {
+      throw new Error(
+        errors.reduce(
+          (msg, e) =>
+            `${msg}\n\nError at ${e.path.length === 0 ? '/' : e.path.join('/')}:\n${
+              e.message
+            }`,
+          ''
+        )
+      )
+    }
+  }
+
+  static load(filename) {
+    let data = yaml.safeLoad(fs.readFileSync(filename, 'utf-8'))
+    Subgraph.validate(data)
     return immutable.fromJS(data)
   }
 
-  static write(subgraph, path) {
-    fs.writeFileSync(path, yaml.safeDump(subgraph.toJS(), { indent: 2 }))
+  static write(subgraph, filename) {
+    fs.writeFileSync(filename, yaml.safeDump(subgraph.toJS(), { indent: 2 }))
   }
 }
