5bc0dcd6db537c5ca8dc46666e952168e01be138
Jannis Pohlmann <contact@jannispohlmann.de>
2018-10-12 11:19:41 +0200

    validation: Validate list field definitions
    We only allow `[Foo!]` or `[Foo!]!` but no null elements inside lists
    like with `[Foo]` or `[Foo]!`.

--- src/validation/schema.js
+++ src/validation/schema.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const graphql = require('graphql/language')
 const immutable = require('immutable')
 
+const List = immutable.List
+const Map = immutable.Map
+
 const loadSchema = filename => {
   try {
     return fs.readFileSync(filename, 'utf-8')
@@ -18,29 +21,64 @@ const parseSchema = doc => {
   }
 }
 
+const validateEntityDirective = def =>
+  def.directives.find(directive => directive.name.value === 'entity')
+    ? List()
+    : immutable.fromJS([
+        {
+          loc: def.loc,
+          message: `Type '${def.name.value}':
+  Defined without @entity directive`,
+        },
+      ])
+
+const validateEntityFieldType = (def, field) =>
+  field.type.kind === 'NonNullType' &&
+  field.type.kind === 'ListType' &&
+  field.type.type.kind !== 'NonNullType'
+    ? immutable.fromJS([
+        {
+          loc: field.loc,
+          message: `\
+Type '${def.name.value}', field ${field.name.value}: \
+Field has type [${field.type.type.name.value}]! but must \
+have type [${field.type.type.name.value}!]!
+  Reason: Lists with null elements are not supported.
+          `,
+        },
+      ])
+    : field.type.kind === 'ListType' && field.type.type.kind !== 'NonNullType'
+      ? immutable.fromJS([
+          {
+            loc: field.loc,
+            message: `\
+Type '${def.name.value}', field ${field.name.value}: \
+Field has type [${field.type.type.name.value}] but must \
+have type [${field.type.type.name.value}!]
+  Reason: Lists with null elements are not supported.
+            `,
+          },
+        ])
+      : List()
+
+const validateEntityFields = def =>
+  def.fields.reduce(
+    (errors, field) => errors.concat(validateEntityFieldType(def, field)),
+    List()
+  )
+
 const typeDefinitionValidators = {
   ObjectTypeDefinition: def =>
-    def.directives.find(directive => directive.name.value === 'entity')
-      ? immutable.fromJS([])
-      : immutable.fromJS([
-          {
-            loc: def.loc,
-            message: `Type '${def.name.value}':
-  Defined without @entity directive`,
-          },
-        ]),
+    validateEntityDirective(def).concat(validateEntityFields(def)),
 }
 
 const validateTypeDefinition = def =>
   typeDefinitionValidators[def.kind] !== undefined
     ? typeDefinitionValidators[def.kind](def)
-    : immutable.fromJS([])
+    : List()
 
 const validateTypeDefinitions = defs =>
-  defs.reduce(
-    (errors, def) => errors.concat(validateTypeDefinition(def)),
-    immutable.fromJS([])
-  )
+  defs.reduce((errors, def) => errors.concat(validateTypeDefinition(def)), List())
 
 const validateSchema = filename => {
   let doc = loadSchema(filename)
