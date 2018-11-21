b49d5f12e503ef6adf3b647dd621626fe17ea703
Jannis Pohlmann <contact@jannispohlmann.de>
2018-11-21 14:58:45 +0100

    subgraph: Validate that all events are present in the data source ABI

--- src/subgraph.js
+++ src/subgraph.js
@@ -125,6 +125,52 @@ module.exports = class Subgraph {
       }, immutable.List())
   }
 
+  static validateEvents(manifest, { resolveFile }) {
+    return manifest
+      .get('dataSources')
+      .filter(dataSource => dataSource.get('kind') === 'ethereum/contract')
+      .reduce((errors, dataSource, dataSourceIndex) => {
+        let path = ['dataSources', dataSourceIndex, 'eventHandlers']
+
+        try {
+          // Resolve the source ABI name into a real ABI object
+          let abiName = dataSource.getIn(['source', 'abi'])
+          let abiEntry = dataSource
+            .getIn(['mapping', 'abis'])
+            .find(abi => abi.get('name') === abiName)
+          let abi = ABI.load(abiEntry.get('name'), resolveFile(abiEntry.get('file')))
+
+          // Obtain event signatures from the mapping
+          let manifestEvents = dataSource
+            .getIn(['mapping', 'eventHandlers'])
+            .map(handler => handler.get('event'))
+
+          // Obtain event signatures from the ABI
+          let abiEvents = abi.eventSignatures()
+
+          // Add errors for every manifest event signature that is not
+          // present in the ABI
+          return manifestEvents.reduce(
+            (errors, manifestEvent, index) =>
+              abiEvents.includes(manifestEvent)
+                ? errors
+                : errors.push({
+                    path: [...path, index],
+                    message:
+                      `Event with signature ${manifestEvent} not present ` +
+                      `in ABI "${abi.name}". ` +
+                      `Candidates: ${abiEvents.toJS()}`,
+                  }),
+            errors
+          )
+        } catch (e) {
+          // Ignore errors silently; we can't really say anything about
+          // the events if the ABI can't even be loaded
+          return errors
+        }
+      }, immutable.List())
+  }
+
   static load(filename) {
     // Load and validate the manifest
     let data = yaml.safeLoad(fs.readFileSync(filename, 'utf-8'))
@@ -144,6 +190,7 @@ module.exports = class Subgraph {
       ...Subgraph.validateSchema(manifest, { resolveFile }),
       ...Subgraph.validateAbis(manifest, { resolveFile }),
       ...Subgraph.validateContractAddresses(manifest),
+      ...Subgraph.validateEvents(manifest, { resolveFile })
     )
 
     if (errors.size > 0) {
