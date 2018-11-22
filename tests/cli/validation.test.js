fb3995d9ce3dc6623b63045e2941e57f33522372
Jannis Pohlmann <contact@jannispohlmann.de>
2018-11-22 18:01:57 +0100

    tests: Add test for ABI not being found in data source

--- tests/cli/validation.test.js
+++ tests/cli/validation.test.js
@@ -1,8 +1,9 @@
 const cliTest = require('./util').cliTest
 
 describe('Validation', () => {
-  cliTest('Invalid manifest', 'validation/invalid-manifest', { exitCode: 1 })
-  cliTest('Invalid ABIs', 'validation/invalid-abis', { exitCode: 1 })
+  cliTest('Invalid subgraph manifest', 'validation/invalid-manifest', { exitCode: 1 })
+  cliTest('ABI not found in data source', 'validation/abi-not-found', { exitCode: 1 })
+  cliTest('Invalid ABI files', 'validation/invalid-abis', { exitCode: 1 })
   cliTest('Event not found in ABI', 'validation/event-not-found', { exitCode: 1 })
   cliTest('Missing entity "id" field', 'validation/missing-entity-id', { exitCode: 1 })
   cliTest('Invalid entity field types', 'validation/invalid-entity-field-types', {
