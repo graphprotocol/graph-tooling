0584c75bad5cec041002662da089676472b513f8
Jannis Pohlmann <contact@jannispohlmann.de>
2019-02-22 16:27:45 +0100

    WIP: commands/init, scaffold: Add subgraph scaffolding using address + Etherscan

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,10 +1,14 @@
 const chalk = require('chalk')
+const fetch = require('node-fetch')
 const os = require('os')
 const path = require('path')
+const toolbox = require('gluegun/toolbox')
 const {
   getSubgraphBasename,
   validateSubgraphName,
 } = require('../command-helpers/subgraph')
+const { withSpinner, step } = require('../command-helpers/spinner')
+const { generateScaffold, writeScaffold } = require('../scaffold')
 
 const HELP = `
 ${chalk.bold('graph init')} [options] ${chalk.bold('<subgraph-name>')}
@@ -12,8 +16,109 @@ ${chalk.bold('graph init')} [options] ${chalk.bold('<subgraph-name>')}
 ${chalk.dim('Options:')}
 
   -h, --help                    Show usage information
-      --allow-simple-name       Use a subgraph name without a prefix component (default: false)
+      --allow-simple-name       Use a subgraph name without a prefix (default: false)
 `
+
+const processInitForm = async (toolbox, subgraphName, { allowSimpleName }) => {
+  let useEtherscan = true
+  let questions = [
+    {
+      type: 'input',
+      name: 'subgraphName',
+      message: 'Subgraph name',
+      initial: subgraphName,
+      validate: name => {
+        try {
+          validateSubgraphName(name, { allowSimpleName })
+          return true
+        } catch (e) {
+          return `${e.message}
+
+  Examples:
+  
+    $ graph init ${os.userInfo().username}/${name}
+    $ graph init ${name} --allow-simple-name`
+        }
+      },
+      result: value => {
+        subgraphName = value
+        return value
+      },
+    },
+    {
+      type: 'input',
+      name: 'directory',
+      message: 'Directory to create the subgraph in',
+      initial: () => getSubgraphBasename(subgraphName),
+      validate: value =>
+        toolbox.filesystem.exists(value) ? 'Directory already exists' : true,
+    },
+    {
+      type: 'select',
+      name: 'network',
+      message: 'Ethereum network',
+      choices: ['mainnet', 'kovan', 'rinkeby', 'ropsten'],
+      initial: 'mainnet',
+    },
+    {
+      type: 'input',
+      name: 'address',
+      message: 'Contract address',
+      validate: value => {
+        // Validate whether the address is valid
+        let pattern = /^(0x)?[0-9a-fA-F]{40}$/
+        if (pattern.test(value)) {
+          return true
+        } else {
+          return `Contract address "${value}" is invalid.
+  Must be 40 hexadecimal characters, with an optional '0x' prefix.`
+        }
+      },
+    },
+    {
+      type: 'confirm',
+      name: 'useEtherscan',
+      message: 'Fetch ABI from Etherscan?',
+      initial: useEtherscan,
+      result: value => {
+        useEtherscan = value
+        return value
+      },
+    },
+    {
+      type: 'input',
+      name: 'abiFile',
+      message: 'ABI file (path)',
+      skip: () => useEtherscan,
+      validate: value =>
+        toolbox.filesystem.exists(value) ? true : 'File does not exist',
+    },
+  ]
+
+  try {
+    return await toolbox.prompt.ask(questions)
+  } catch (e) {
+    return undefined
+  }
+}
+
+const loadAbiFromEtherscan = async (network, address) => {
+  let result = await fetch(
+    `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}`
+  )
+  let json = await result.json()
+  return JSON.parse(json.result)
+}
+
+const loadAbiFromFile = async file => await toolbox.filesystem.read(abiFile, 'json')
+
+const loadAbi = async ({ abiFile, address, network, useEtherscan }) => {
+  if (useEtherscan) {
+    return await loadAbiFromEtherscan(network, address)
+  } else {
+    return await loadAbiFromFile(abiFile)
+  }
+}
 
 module.exports = {
   description: 'Creates a new subgraph with basic scaffolding',
@@ -28,37 +133,6 @@ module.exports = {
     // Show help text if requested
     if (h || help) {
       print.info(HELP)
-      return
-    }
-
-    // Validate the subgraph name
-    if (!subgraphName) {
-      print.error('No subgraph name provided')
-      print.info(HELP)
-      process.exitCode = 1
-      return
-    }
-    try {
-      validateSubgraphName(subgraphName, { allowSimpleName })
-    } catch (e) {
-      print.error(e.message)
-      print.info(`
-Examples:
-
-  $ graph init ${os.userInfo().username}/${subgraphName}
-  $ graph init ${subgraphName} --allow-simple-name`)
-      process.exitCode = 1
-      return
-    }
-
-    // Use the subgraph name as the directory name
-    let directory = getSubgraphBasename(subgraphName)
-    let relativeDirectory = path.relative(process.cwd(), directory)
-
-    // Fail if the output directory alread exists
-    if (toolbox.filesystem.exists(directory)) {
-      print.error(`Directory or file "${directory}" already exists`)
-      process.exitCode = 1
       return
     }
 
@@ -83,82 +157,127 @@ Examples:
       return
     }
 
+    // Collect user input
+    let inputs = await processInitForm(toolbox, subgraphName, { allowSimpleName })
+    if (inputs === undefined) {
+      process.exit(1)
+      return
+    }
+
+    print.info('———')
+
+    // Extract user input
+    subgraphName = inputs.subgraphName
+    let { abiFile, address, directory, network, useEtherscan } = inputs
+
     let installCommand = yarn ? 'yarn' : 'npm install'
     let codegenCommand = yarn ? 'yarn codegen' : 'npm run codegen'
     let deployCommand = yarn ? 'yarn deploy' : 'npm run deploy'
 
-    // Clone the example subgraph repository
-    let spinner
-    try {
-      spinner = print.spin('Cloning example subgraph')
-      await system.run(
-        `git clone http://github.com/graphprotocol/example-subgraph ${directory}`
-      )
-      spinner.stop()
-    } catch (e) {
-      spinner.fail(`Failed to clone example subgraph repository: ${e}`)
-      process.exitCode = 1
-      return
-    }
-
-    // Update package.json to match the subgraph name
-    try {
-      // Load package.json
-      let pkgJsonFilename = filesystem.path(directory, 'package.json')
-      let pkgJson = await filesystem.read(pkgJsonFilename, 'json')
-
-      pkgJson.name = getSubgraphBasename(subgraphName)
-      Object.keys(pkgJson.scripts).forEach(name => {
-        pkgJson.scripts[name] = pkgJson.scripts[name].replace('example', subgraphName)
-      })
-      delete pkgJson['license']
-      delete pkgJson['repository']
-
-      // Write package.json
-      await filesystem.write(pkgJsonFilename, pkgJson, { jsonIndent: 2 })
-    } catch (e) {
-      print.error(`Failed to preconfigure the subgraph: ${e}`)
-      process.exitCode = 1
-      filesystem.remove(directory)
-      return
-    }
-
-    // Reset the git repository
-    try {
-      spinner = print.spin('Creating initial commit')
-      await filesystem.remove(filesystem.path(directory, '.git'))
-      await system.run('git init', { cwd: directory })
-      await system.run('git add --all', { cwd: directory })
-      await system.run('git commit -m "Initial commit"', {
-        cwd: directory,
-      })
-      spinner.stop()
-    } catch (e) {
-      spinner.fail(
-        `Failed to initialize the subgraph directory ${relativeDirectory}: ${e}`
-      )
-      process.exitCode = 1
-      filesystem.remove(directory)
-      return
-    }
-
-    print.success(
-      `Subgraph "${subgraphName}" created in ${relativeDirectory}${filesystem.separator}`
+    // Obtain the ABI from the network-specific Etherscan or from the local file
+    let abi = await withSpinner(
+      `Load ABI from ${useEtherscan ? 'Etherscan' : abiFile}`,
+      `Failed to load ABI from ${useEtherscan ? 'Etherscan' : abiFile}`,
+      async spinner => {
+        return await loadAbi({ abiFile, address, network, useEtherscan })
+      }
     )
-    print.info(`
-Next steps:
+    if (abi === undefined) {
+      process.exitCode = 1
+      return
+    }
+
+    // Fail if the output directory already exists
+    if (toolbox.filesystem.exists(directory)) {
+      print.error(`Directory or file "${directory}" already exists`)
+      process.exitCode = 1
+      return
+    }
+
+    // Scaffold subgraph from ABI
+    let scaffold = await withSpinner(
+      `Create subgraph scaffold`,
+      `Failed to create subgraph scaffold`,
+      async spinner => {
+        let scaffold = await generateScaffold(
+          {
+            subgraphName,
+            abi,
+            network,
+            address,
+          },
+          spinner
+        )
+        await writeScaffold(scaffold, directory, spinner)
+        return true
+      }
+    )
+    if (scaffold !== true) {
+      process.exitCode = 1
+      return
+    }
+
+    // Initialize a git repository
+    let repo = await withSpinner(
+      `Initialize subgraph repository`,
+      `Failed to initialize subgraph repository`,
+      async spinner => {
+        await system.run('git init', { cwd: directory })
+        await system.run('git add --all', { cwd: directory })
+        await system.run('git commit -m "Initial commit"', {
+          cwd: directory,
+        })
+        return true
+      }
+    )
+    if (repo !== true) {
+      process.exitCode = 1
+      return
+    }
+
+    // Install dependencies
+    let installed = await withSpinner(
+      `Install dependencies with \`${installCommand}\``,
+      `Failed to install dependencies`,
+      async spinner => {
+        await system.run(installCommand, { cwd: directory })
+        return true
+      }
+    )
+    if (installed !== true) {
+      process.exitCode = 1
+      return
+    }
+
+    // Run code-generation
+    let codegen = await withSpinner(
+      `Generate ABI and schema types with \`${codegenCommand}\``,
+      `Failed to generatecode from ABI and GraphQL schema`,
+      async spinner => {
+        await system.run(codegenCommand, { cwd: directory })
+        return true
+      }
+    )
+    if (codegen !== true) {
+      process.exitCode = 1
+      return
+    }
+
+    // Print instructions
+    print.success(
+      `
+Subgraph "${subgraphName}" created in ${directory}${filesystem.separator}
+`
+    )
+    print.info(`Next steps:
     
-  1. Run \`graph auth https://api.thegraph.com/deploy/ <your access token>\`
+  1. Run \`graph auth https://api.thegraph.com/deploy/ <access-token>\`
      to authenticate with the hosted service. You can get the access token from
      https://thegraph.com/explorer/dashboard/.
   
   2. Type \`cd ${directory}\` to enter the subgraph.
   
-  3. Run \`${installCommand}\` to install dependencies.
-  
-  4. Run \`${codegenCommand}\` to generate code from contract ABIs and the GraphQL schema.
-  
-  5. Run \`${deployCommand}\` to deploy the subgraph to
+  3. Run \`${deployCommand}\` to deploy the subgraph to
      https://thegraph.com/explorer/subgraph/${subgraphName}.
 
 Make sure to visit the documentation on https://thegraph.com/docs/ for further information.`)
---
+++ src/scaffold.js
@@ -0,0 +1,192 @@
+const fs = require('fs-extra')
+const path = require('path')
+const prettier = require('prettier')
+const { getSubgraphBasename } = require('./command-helpers/subgraph')
+const { step } = require('./command-helpers/spinner')
+
+const abiEvents = abi => abi.filter(item => item.type === 'event')
+
+// package.json
+
+const generatePackageJson = ({ subgraphName }) =>
+  prettier.format(
+    JSON.stringify({
+      name: getSubgraphBasename(subgraphName),
+      license: 'UNLICENSED',
+      scripts: {
+        codegen: 'graph codegen',
+        build: 'graph build',
+        deploy:
+          `graph deploy ` +
+          `--node https://api.thegraph.com/deploy/ ` +
+          `--ipfs https://api.thegraph.com/ipfs/ ` +
+          subgraphName,
+        'create-local': `graph create --node http://localhost:8020/ ${subgraphName}`,
+        'remove-local': `graph remove --node http://localhost:8020/ ${subgraphName}`,
+        'deploy-local':
+          `graph deploy ` +
+          `--node http://localhost:8020/ ` +
+          `--ipfs http://localhost:5001 ` +
+          subgraphName,
+      },
+      dependencies: {
+        '@graphprotocol/graph-cli': '0.7.2',
+        '@graphprotocol/graph-ts': '0.5.1',
+      },
+    }),
+    { parser: 'json' }
+  )
+
+// Subgraph manifest
+
+const eventSignature = event =>
+  `${event.name}(${event.inputs.map(input => input.type).join(',')})`
+
+const generateManifest = ({ abi, address, network, subgraphName }) =>
+  prettier.format(
+    `
+specVersion: 0.0.1
+schema:
+  file: ./schema.graphql
+dataSources:
+  - kind: ethereum/contract
+    name: Contract
+    network: ${network}
+    source:
+      address: '${address}'
+      abi: Contract
+    mapping:
+      kind: ethereum/events
+      apiVersion: 0.0.1
+      language: wasm/assemblyscript
+      entities:
+        ${abiEvents(abi)
+          .map(event => `- ${event.name}`)
+          .join('\n        ')}
+      abis:
+        - name: Contract
+          file: ./abis/Contract.json
+      eventHandlers:
+        ${abiEvents(abi)
+          .map(
+            event => `
+        - event: ${eventSignature(event)}
+          handler: handle${event.name}`
+          )
+          .join('')}
+      file: ./src/mapping.ts
+`,
+    { parser: 'yaml' }
+  )
+
+// Schema
+
+// FIXME: Use the type logic in ./codegen/types
+const ETHEREUM_TYPES = {
+  address: 'Bytes',
+  bytes: 'Bytes',
+  bytes4: 'Bytes',
+  bytes32: 'Bytes',
+  string: 'String',
+  uint256: 'BigInt',
+}
+
+const ethereumTypeToGraphQL = name => ETHEREUM_TYPES[name]
+
+const generateEventType = event => `type ${event.name} @entity {
+      id: ID!
+      ${event.inputs
+        // TODO: Support inputs without names
+        .map(
+          input => `${input.name}: ${ethereumTypeToGraphQL(input.type)}! # ${input.type}`
+        )
+        .join('\n')}
+    }`
+
+const generateSchema = ({ abi }) =>
+  prettier.format(
+    abiEvents(abi)
+      .map(generateEventType)
+      .join('\n'),
+    {
+      parser: 'graphql',
+    }
+  )
+
+// Mapping
+
+const generateMapping = ({ abi, subgraphName }) =>
+  prettier.format(
+    `
+  import { ${abiEvents(abi).map(
+    event => `${event.name} as ${event.name}Event`
+  )}} from '../generated/Contract/Contract'
+  import { ${abiEvents(abi).map(event => event.name)} } from '../generated/schema'
+  
+  ${abiEvents(abi)
+    .map(
+      event =>
+        `
+  export function handle${event.name}(event: ${event.name}Event): void {
+    let entity = new ${
+      event.name
+    }(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+    ${event.inputs
+      // FIXME: Support inputs without name
+      .map(input => `entity.${input.name} = event.params.${input.name}`)
+      .join('\n')}
+    entity.save()
+  }
+    `
+    )
+    .join('\n')}
+  `,
+    { parser: 'typescript' }
+  )
+
+const generateScaffold = async ({ abi, address, network, subgraphName }, spinner) => {
+  step(spinner, 'Generate subgraph from ABI')
+  let packageJson = generatePackageJson({ subgraphName })
+  let manifest = generateManifest({ abi, address, network, subgraphName })
+  let schema = generateSchema({ abi })
+  let mapping = generateMapping({ abi, subgraphName })
+
+  return {
+    'package.json': packageJson,
+    'subgraph.yaml': manifest,
+    'schema.graphql': schema,
+    src: { 'mapping.ts': mapping },
+    abis: {
+      'Contract.json': prettier.format(JSON.stringify(abi), {
+        parser: 'json',
+      }),
+    },
+  }
+}
+
+const writeScaffoldDirectory = async (scaffold, directory, spinner) => {
+  // Create directory itself
+  fs.mkdirsSync(directory)
+
+  Object.keys(scaffold).forEach(basename => {
+    let content = scaffold[basename]
+    let filename = path.join(directory, basename)
+
+    // Write file or recurse into subdirectory
+    if (typeof content === 'string') {
+      fs.writeFileSync(filename, content, { encoding: 'utf-8' })
+    } else {
+      writeScaffoldDirectory(content, path.join(directory, basename), spinner)
+    }
+  })
+}
+
+const writeScaffold = async (scaffold, directory, spinner) => {
+  step(spinner, `Write subgraph to directory`)
+  await writeScaffoldDirectory(scaffold, directory, spinner)
+}
+
+module.exports = {
+  generateScaffold,
+  writeScaffold,
+}
