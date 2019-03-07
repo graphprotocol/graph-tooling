aebb8f09a1551b49304009df12a8fa86c1218795
Jannis Pohlmann <contact@jannispohlmann.de>
2019-03-07 13:08:01 +0100

    tests/cli/validation: Pass 'codegen' command to cliTest

--- tests/cli/validation.test.js
+++ tests/cli/validation.test.js
@@ -1,14 +1,33 @@
 const cliTest = require('./util').cliTest
 
 describe('Validation', () => {
-  cliTest('Invalid subgraph manifest', 'validation/invalid-manifest', { exitCode: 1 })
-  cliTest('ABI not found in data source', 'validation/abi-not-found', { exitCode: 1 })
-  cliTest('Invalid ABI files', 'validation/invalid-abis', { exitCode: 1 })
-  cliTest('Event not found in ABI', 'validation/event-not-found', { exitCode: 1 })
-  cliTest('Missing entity "id" field', 'validation/missing-entity-id', { exitCode: 1 })
-  cliTest('Invalid entity field types', 'validation/invalid-entity-field-types', {
+  cliTest('Invalid subgraph manifest', ['codegen'], 'validation/invalid-manifest', {
     exitCode: 1,
   })
-  cliTest('Invalid contract addresses', 'validation/invalid-contract-addresses')
-  cliTest('Entity field arguments', 'validation/entity-field-args', { exitCode: 1 })
+  cliTest('ABI not found in data source', ['codegen'], 'validation/abi-not-found', {
+    exitCode: 1,
+  })
+  cliTest('Invalid ABI files', ['codegen'], 'validation/invalid-abis', { exitCode: 1 })
+  cliTest('Event not found in ABI', ['codegen'], 'validation/event-not-found', {
+    exitCode: 1,
+  })
+  cliTest('Missing entity "id" field', ['codegen'], 'validation/missing-entity-id', {
+    exitCode: 1,
+  })
+  cliTest(
+    'Invalid entity field types',
+    ['codegen'],
+    'validation/invalid-entity-field-types',
+    {
+      exitCode: 1,
+    }
+  )
+  cliTest(
+    'Invalid contract addresses',
+    ['codegen'],
+    'validation/invalid-contract-addresses'
+  )
+  cliTest('Entity field arguments', ['codegen'], 'validation/entity-field-args', {
+    exitCode: 1,
+  })
 })
