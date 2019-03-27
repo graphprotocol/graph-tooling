wl-datamapping/graph-cli

*: Update withSpinner to handle warning messages, use for loadSubgraph()
Create buildCombinedWarning() to join loadSubgraph() warnings.
Use buildCombinedWarning() and updated withSpinner() to display
warning messages from loadSubgraph() and continue process.

src/command-helpers/spinner.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk')
+const immutable = require('immutable')
 const toolbox = require('gluegun/toolbox')
 
 const step = (spinner, subject, text) => {
@@ -13,12 +14,22 @@ const step = (spinner, subject, text) => {
   return spinner
 }
 
-const withSpinner = async (text, errorText, f) => {
+const withSpinner = async (text, errorText, warningText, f) => {
   let spinner = toolbox.print.spin(text)
   try {
     let result = await f(spinner)
-    spinner.succeed(text)
-    return result
+    if(warningText && immutable.Map.isMap(result)) {
+      if (result.get('warning', false)) {
+        spinner.stopAndPersist({symbol: '⚠️', text: `${warningText}: ${result.get('warning')}`})
+        return result.get('value')
+      } else {
+        spinner.succeed(text)
+        return result.get('value', result)
+      }
+    } else {
+      spinner.succeed(text)
+      return result
+    }
   } catch (e) {
     spinner.fail(`${errorText}: ${e.message}`)
     throw e
@@ -27,5 +38,5 @@ const withSpinner = async (text, errorText, f) => {
 
 module.exports = {
   step,
-  withSpinner,
+  withSpinner
 }

src/subgraph.js
@@ -23,21 +23,21 @@ const throwCombinedError = (filename, errors) => {
   )
 }
 
-const throwCombinedWarning = (filename, warnings) => {
-  toolbox.print.info('')
-  toolbox.print.warning(
-    warnings.reduce(
-      (msg, w) =>
-        `${msg}
-
-  Path: ${w.get('path').size === 0 ? '/' : w.get('path').join(' > ')}
-  ${w
-          .get('message')
-          .split('\n')
-          .join('\n  ')}`,
-      `Warning in ${path.relative(process.cwd(), filename)}:`
-    )
-  )
+const buildCombinedWarning = (filename, warnings) => {
+  return (warnings.size > 0)
+    ?
+      warnings.reduce(
+        (msg, w) =>
+          `${msg}
+  
+    Path: ${w.get('path').size === 0 ? '/' : w.get('path').join(' > ')}
+    ${w
+            .get('message')
+            .split('\n')
+            .join('\n  ')}`,
+        `Warning in ${path.relative(process.cwd(), filename)}:`
+      )
+    : null
 }
 
 module.exports = class Subgraph {
@@ -274,19 +274,17 @@ ${abiEvents
 
     // Perform warning validations
     let warnings = immutable.List.of(
-      ...Subgraph.validateRepository(manifest, { resolveFile }),
-      ...Subgraph.validateDescription(manifest, { resolveFile })
+      ...Subgraph.validateDescription(manifest, { resolveFile }),
+      ...Subgraph.validateRepository(manifest, { resolveFile })
     )
-
-    if (warnings.size > 0) {
-      throwCombinedWarning(filename, warnings)
-    }
 
     if (errors.size > 0) {
       throwCombinedError(filename, errors)
     }
-
-    return manifest
+    return immutable.Map({
+      value: manifest,
+      warning: buildCombinedWarning(filename, warnings)
+    })
   }
 
   static write(subgraph, filename) {

graphprotocol/graph-tooling

src/command-helpers/spinner.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk')
+const immutable = require('immutable')
 const toolbox = require('gluegun/toolbox')
 
 const step = (spinner, subject, text) => {
@@ -13,12 +14,24 @@ const step = (spinner, subject, text) => {
   return spinner
 }
 
-const withSpinner = async (text, errorText, f) => {
+// A wrapper for functions (f) that provides a progress spinner and formatted alert messages.
+// For warning message support the f must return an immutable Map with result and warning keys.
+const withSpinner = async (text, errorText, warningText, f) => {
   let spinner = toolbox.print.spin(text)
   try {
     let result = await f(spinner)
-    spinner.succeed(text)
-    return result
+    if(warningText && immutable.Map.isMap(result)) {
+      if (result.get('warning', false)) {
+        spinner.stopAndPersist({symbol: '⚠️', text: `${warningText}: ${result.get('warning')}`})
+        return result.get('result')
+      } else {
+        spinner.succeed(text)
+        return result.get('result', result)
+      }
+    } else {
+      spinner.succeed(text)
+      return result
+    }
   } catch (e) {
     spinner.fail(`${errorText}: ${e.message}`)
     throw e
@@ -27,5 +40,5 @@ const withSpinner = async (text, errorText, f) => {
 
 module.exports = {
   step,
-  withSpinner,
+  withSpinner
 }

src/subgraph.js
@@ -23,21 +23,21 @@ const throwCombinedError = (filename, errors) => {
   )
 }
 
-const throwCombinedWarning = (filename, warnings) => {
-  toolbox.print.info('')
-  toolbox.print.warning(
-    warnings.reduce(
-      (msg, w) =>
-        `${msg}
-
-  Path: ${w.get('path').size === 0 ? '/' : w.get('path').join(' > ')}
-  ${w
-          .get('message')
-          .split('\n')
-          .join('\n  ')}`,
-      `Warning in ${path.relative(process.cwd(), filename)}:`
-    )
-  )
+const buildCombinedWarning = (filename, warnings) => {
+  return (warnings.size > 0)
+    ?
+      warnings.reduce(
+        (msg, w) =>
+          `${msg}
+  
+    Path: ${w.get('path').size === 0 ? '/' : w.get('path').join(' > ')}
+    ${w
+            .get('message')
+            .split('\n')
+            .join('\n  ')}`,
+        `Warning in ${path.relative(process.cwd(), filename)}:`
+      )
+    : null
 }
 
 module.exports = class Subgraph {
@@ -274,19 +274,17 @@ ${abiEvents
 
     // Perform warning validations
     let warnings = immutable.List.of(
-      ...Subgraph.validateRepository(manifest, { resolveFile }),
-      ...Subgraph.validateDescription(manifest, { resolveFile })
+      ...Subgraph.validateDescription(manifest, { resolveFile }),
+      ...Subgraph.validateRepository(manifest, { resolveFile })
     )
-
-    if (warnings.size > 0) {
-      throwCombinedWarning(filename, warnings)
-    }
 
     if (errors.size > 0) {
       throwCombinedError(filename, errors)
     }
-
-    return manifest
+    return immutable.Map({
+      result: manifest,
+      warning: buildCombinedWarning(filename, warnings)
+    })
   }
 
   static write(subgraph, filename) {