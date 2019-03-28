ef47fb7f18f5606c3e341c1b3dfa7c02994b02a9
Jannis <jannis@thegraph.com>
2019-03-28 18:37:05 +0100

    tests: Use --skip-migrations in validation tests

--- tests/cli/validation.test.js
+++ tests/cli/validation.test.js
@@ -1,39 +1,69 @@
 const cliTest = require('./util').cliTest
 
 describe('Validation', () => {
-  cliTest('Invalid subgraph manifest', ['codegen'], 'validation/invalid-manifest', {
-    exitCode: 1,
-  })
-  cliTest('ABI not found in data source', ['codegen'], 'validation/abi-not-found', {
-    exitCode: 1,
-  })
-  cliTest('Invalid ABI files', ['codegen'], 'validation/invalid-abis', { exitCode: 1 })
-  cliTest('Event not found in ABI', ['codegen'], 'validation/event-not-found', {
-    exitCode: 1,
-  })
-  cliTest('Missing entity "id" field', ['codegen'], 'validation/missing-entity-id', {
-    exitCode: 1,
-  })
+  cliTest(
+    'Invalid subgraph manifest',
+    ['codegen', '--skip-migrations'],
+    'validation/invalid-manifest',
+    {
+      exitCode: 1,
+    },
+  )
+  cliTest(
+    'ABI not found in data source',
+    ['codegen', '--skip-migrations'],
+    'validation/abi-not-found',
+    {
+      exitCode: 1,
+    },
+  )
+  cliTest(
+    'Invalid ABI files',
+    ['codegen', '--skip-migrations'],
+    'validation/invalid-abis',
+    { exitCode: 1 },
+  )
+  cliTest(
+    'Event not found in ABI',
+    ['codegen', '--skip-migrations'],
+    'validation/event-not-found',
+    {
+      exitCode: 1,
+    },
+  )
+  cliTest(
+    'Missing entity "id" field',
+    ['codegen', '--skip-migrations'],
+    'validation/missing-entity-id',
+    {
+      exitCode: 1,
+    },
+  )
   cliTest(
     'Invalid entity field types',
-    ['codegen'],
+    ['codegen', '--skip-migrations'],
     'validation/invalid-entity-field-types',
     {
       exitCode: 1,
-    }
+    },
   )
   cliTest(
     'Invalid contract addresses',
-    ['codegen'],
-    'validation/invalid-contract-addresses'
+    ['codegen', '--skip-migrations'],
+    'validation/invalid-contract-addresses',
   )
-  cliTest('Entity field arguments', ['codegen'], 'validation/entity-field-args', {
-    exitCode: 1,
-  })
+  cliTest(
+    'Entity field arguments',
+    ['codegen', '--skip-migrations'],
+    'validation/entity-field-args',
+    {
+      exitCode: 1,
+    },
+  )
   cliTest(
     'Example values found in manifest',
-    ['codegen'],
+    ['codegen', '--skip-migrations'],
     'validation/example-values-found',
-    { exitCode: 0 }
+    { exitCode: 0 },
   )
 })
