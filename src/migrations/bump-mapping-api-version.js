63c995c147c762d82209b0bfa1286a71a96afccd
Jannis <jannis@thegraph.com>
2019-04-03 16:50:28 +0200

    migrations: Fix graph-ts version for the apiVersion bump to > 0.5.1 as intended

--- src/migrations/bump-mapping-api-version.js
+++ src/migrations/bump-mapping-api-version.js
@@ -34,7 +34,7 @@ module.exports = {
     let manifest = yaml.safeLoad(fs.readFileSync(manifestFile, 'utf-8'))
     return (
       // Only migrate if the graph-ts version is > 0.5.1...
-      semver.gt(graphTsVersion, '0.5.0') &&
+      semver.gt(graphTsVersion, '0.5.1') &&
       // ...and we have a manifest with mapping > apiVersion = 0.0.1
       manifest &&
       typeof manifest === 'object' &&
