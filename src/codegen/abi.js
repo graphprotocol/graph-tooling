eba03ead4f9be14fb7b405861cc829afb49af5d4
Jannis Pohlmann <jannis@thegraph.com>
2019-05-08 21:07:08 +0200

    codegen/abi: Use the new `disambiguateNames` helper in ABI codegen
    This guarantees that the following are always disambiguated:
    
      * `EthereumCall` subclasses for traceable functions
      * Inputs and outputs of these functions
      * `EthereumEvent` subclasses for contract events
      * Inputs and outputs of these events

--- src/codegen/abi.js
+++ src/codegen/abi.js
@@ -2,6 +2,7 @@ const immutable = require('immutable')
 
 const tsCodegen = require('./typescript')
 const typesCodegen = require('./types')
+const util = require('./util')
 
 module.exports = class AbiCodeGenerator {
   constructor(abi) {
@@ -41,18 +42,21 @@ module.exports = class AbiCodeGenerator {
   }
 
   _generateCallTypes() {
-    return this.abi
-      .callFunctions()
+    let callFunctions = util.disambiguateNames({
+      values: this.abi.callFunctions(),
+      getName: fn =>
+        fn.get('name') || (fn.get('type') === 'constructor' ? 'constructor' : 'default'),
+      setName: (fn, name) => fn.set('_alias', name),
+    })
+
+    return callFunctions
       .map(fn => {
-        let fnName = fn.get(
-          'name',
-          fn.get('type') === 'constructor' ? 'constructor' : 'default',
-        )
-        let fnClassName = `${fnName.charAt(0).toUpperCase()}${fnName.slice(1)}Call`
+        let fnAlias = fn.get('_alias')
+        let fnClassName = `${fnAlias.charAt(0).toUpperCase()}${fnAlias.slice(1)}Call`
         let tupleClasses = []
 
         // First, generate a class with the input getters
-        let inputsClassName = fnClassName + 'Inputs'
+        let inputsClassName = fnClassName + '__Inputs'
         let inputsClass = tsCodegen.klass(inputsClassName, { export: true })
         inputsClass.addMember(tsCodegen.klassMember('_call', fnClassName))
         inputsClass.addMethod(
@@ -65,20 +69,26 @@ module.exports = class AbiCodeGenerator {
         )
 
         // Generate getters and classes for function inputs
-        fn.get('inputs', immutable.List()).forEach((input, index) => {
-          let callInput = this._generateInputOrOutput(
-            input,
-            index,
-            fnClassName,
-            `call`,
-            `inputValues`,
-          )
-          inputsClass.addMethod(callInput.getter)
-          tupleClasses.push(...callInput.classes)
-        })
+        util
+          .disambiguateNames({
+            values: fn.get('inputs', immutable.List()),
+            getName: (input, index) => input.get('name') || `value${index}`,
+            setName: (input, name) => input.set('name', name),
+          })
+          .forEach((input, index) => {
+            let callInput = this._generateInputOrOutput(
+              input,
+              index,
+              fnClassName,
+              `call`,
+              `inputValues`,
+            )
+            inputsClass.addMethod(callInput.getter)
+            tupleClasses.push(...callInput.classes)
+          })
 
         // Second, generate a class with the output getters
-        let outputsClassName = fnClassName + 'Outputs'
+        let outputsClassName = fnClassName + '__Outputs'
         let outputsClass = tsCodegen.klass(outputsClassName, { export: true })
         outputsClass.addMember(tsCodegen.klassMember('_call', fnClassName))
         outputsClass.addMethod(
@@ -91,17 +101,23 @@ module.exports = class AbiCodeGenerator {
         )
 
         // Generate getters and classes for function outputs
-        fn.get('outputs', immutable.List()).forEach((output, index) => {
-          let callInput = this._generateInputOrOutput(
-            output,
-            index,
-            fnClassName,
-            `call`,
-            `outputValues`,
-          )
-          outputsClass.addMethod(callInput.getter)
-          tupleClasses.push(...callInput.classes)
-        })
+        util
+          .disambiguateNames({
+            values: fn.get('outputs', immutable.List()),
+            getName: (output, index) => output.get('name') || `value${index}`,
+            setName: (output, name) => output.set('name', name),
+          })
+          .forEach((output, index) => {
+            let callInput = this._generateInputOrOutput(
+              output,
+              index,
+              fnClassName,
+              `call`,
+              `outputValues`,
+            )
+            outputsClass.addMethod(callInput.getter)
+            tupleClasses.push(...callInput.classes)
+          })
 
         // Then, generate the event class itself
         let klass = tsCodegen.klass(fnClassName, {
@@ -134,20 +150,16 @@ module.exports = class AbiCodeGenerator {
   }
 
   _generateEventTypes() {
-    // Keep a map of how many events were generated so far with the given name.
-    // If there are no name collisions, all entries map to 1.
-    let generatedEvents = new Map()
-    return this.abi.data
-      .filter(member => member.get('type') === 'event')
+    // Enumerate events with duplicate names
+    let events = util.disambiguateNames({
+      values: this.abi.data.filter(member => member.get('type') === 'event'),
+      getName: event => event.get('name'),
+      setName: (event, name) => event.set('_alias', name),
+    })
+
+    return events
       .map(event => {
-        let eventClassName = event.get('name')
-        let currentCount = generatedEvents.get(eventClassName)
-        if (currentCount === undefined) {
-          generatedEvents.set(eventClassName, 1)
-        } else {
-          generatedEvents.set(eventClassName, currentCount + 1)
-          eventClassName += currentCount
-        }
+        let eventClassName = event.get('_alias')
         let tupleClasses = []
 
         // First, generate a class with the param getters
@@ -163,7 +175,14 @@ module.exports = class AbiCodeGenerator {
           ),
         )
 
-        event.get('inputs').forEach((input, index) => {
+        // Enumerate inputs with duplicate names
+        let inputs = util.disambiguateNames({
+          values: event.get('inputs'),
+          getName: (input, index) => input.get('name') || `param${index}`,
+          setName: (input, name) => input.set('name', name),
+        })
+
+        inputs.forEach((input, index) => {
           // Generate getters and classes for event params
           let paramObject = this._generateInputOrOutput(
             input,
@@ -310,138 +329,151 @@ module.exports = class AbiCodeGenerator {
       ),
     )
 
-    this.abi.data.forEach(member => {
-      switch (member.get('type')) {
-        case 'function':
-          if (
-            member.get('stateMutability') === 'view' ||
-            member.get('stateMutability') === 'pure'
-          ) {
-            let methodName = member.get('name')
-            
-            // Generate a type for the result of calling the function
-            let returnType = undefined
-            let simpleReturnType = true
-            if (member.get('outputs').size > 1) {
-              simpleReturnType = false
+    // Get view/pure functions from the contract
+    let functions = this.abi.data.filter(
+      member =>
+        member.get('type') === 'function' &&
+        (member.get('stateMutability') === 'view' ||
+          member.get('stateMutability') === 'pure'),
+    )
 
-              // Create a type dedicated to holding the return values
-              returnType = tsCodegen.klass(
-                this.abi.name + '__' + methodName + 'Result',
-                { export: true },
+    // Disambiguate functions with duplicate names
+    functions = util.disambiguateNames({
+      values: functions,
+      getName: fn => fn.get('name'),
+      setName: (fn, name) => fn.set('_alias', name),
+    })
+
+    functions.forEach(member => {
+      let fnName = member.get('name')
+      let fnAlias = member.get('_alias')
+
+      // Generate a type for the result of calling the function
+      let returnType = undefined
+      let simpleReturnType = true
+      if (member.get('outputs', immutable.List()).size > 1) {
+        simpleReturnType = false
+
+        // Create a type dedicated to holding the return values
+        returnType = tsCodegen.klass(this.abi.name + '__' + fnAlias + 'Result', {
+          export: true,
+        })
+
+        // Add a constructor to this type
+        returnType.addMethod(
+          tsCodegen.method(
+            'constructor',
+            member
+              .get('outputs')
+              .map((output, index) =>
+                tsCodegen.param(
+                  `value${index}`,
+                  typesCodegen.ascTypeForEthereum(output.get('type')),
+                ),
+              ),
+            null,
+            member
+              .get('outputs')
+              .map((output, index) => `this.value${index} = value${index}`)
+              .join('\n'),
+          ),
+        )
+
+        // Add a `toMap(): TypedMap<string,EthereumValue>` function to the return type
+        returnType.addMethod(
+          tsCodegen.method(
+            'toMap',
+            [],
+            tsCodegen.namedType('TypedMap<string,EthereumValue>'),
+            `
+            let map = new TypedMap<string,EthereumValue>();
+            ${member
+              .get('outputs')
+              .map(
+                (output, index) =>
+                  `map.set('value${index}', ${typesCodegen.ethereumValueFromAsc(
+                    `this.value${index}`,
+                    output.get('type'),
+                  )})`,
               )
+              .join(';')}
+            return map;
+            `,
+          ),
+        )
 
-              // Add a constructor to this type
-              returnType.addMethod(
-                tsCodegen.method(
-                  'constructor',
-                  member
-                    .get('outputs')
-                    .map((output, index) =>
-                      tsCodegen.param(
-                        `value${index}`,
-                        typesCodegen.ascTypeForEthereum(output.get('type')),
+        // Add value0, value1 etc. members to the type
+        member
+          .get('outputs')
+          .map((output, index) =>
+            tsCodegen.klassMember(
+              `value${index}`,
+              typesCodegen.ascTypeForEthereum(output.get('type')),
+            ),
+          )
+          .forEach(member => returnType.addMember(member))
+
+        // Add the type to the types we'll create
+        types = types.push(returnType)
+
+        returnType = tsCodegen.namedType(returnType.name)
+      } else {
+        returnType = tsCodegen.namedType(
+          typesCodegen.ascTypeForEthereum(
+            member
+              .get('outputs')
+              .get(0)
+              .get('type'),
+          ),
+        )
+      }
+
+      // Disambiguate inputs with duplicate names
+      let inputs = util.disambiguateNames({
+        values: member.get('inputs', immutable.List()),
+        getName: (input, index) => input.get('name') || `param${index}`,
+        setName: (input, name) => input.set('_alias', name),
+      })
+
+      // Generate and add a method that implements calling the function on
+      // the smart contract
+      klass.addMethod(
+        tsCodegen.method(
+          fnAlias,
+          inputs.map(input =>
+            tsCodegen.param(
+              input.get('_alias'),
+              typesCodegen.ascTypeForEthereum(input.get('type')),
+            ),
+          ),
+          returnType,
+          `
+          let result = super.call(
+            '${fnName}',
+            [${
+              inputs.size > 0
+                ? inputs
+                    .map(input =>
+                      typesCodegen.ethereumValueFromAsc(
+                        input.get('_alias'),
+                        input.get('type'),
                       ),
-                    ),
-                  null,
-                  member
-                    .get('outputs')
-                    .map((output, index) => `this.value${index} = value${index}`)
-                    .join('\n'),
-                ),
-              )
-
-              // Add a `toMap(): TypedMap<string,EthereumValue>` function to the return type
-              returnType.addMethod(
-                tsCodegen.method(
-                  'toMap',
-                  [],
-                  tsCodegen.namedType('TypedMap<string,EthereumValue>'),
-                  `
-                  let map = new TypedMap<string,EthereumValue>();
-                  ${member
-                    .get('outputs')
-                    .map(
-                      (output, index) =>
-                        `map.set('value${index}', ${typesCodegen.ethereumValueFromAsc(
-                          `this.value${index}`,
-                          output.get('type'),
-                        )})`,
                     )
-                    .join(';')}
-                  return map;
-                  `,
-                ),
-              )
-
-              // Add value0, value1 etc. members to the type
-              member
-                .get('outputs')
-                .map((output, index) =>
-                  tsCodegen.klassMember(
-                    `value${index}`,
-                    typesCodegen.ascTypeForEthereum(output.get('type')),
-                  ),
-                )
-                .forEach(member => returnType.addMember(member))
-
-              // Add the type to the types we'll create
-              types = types.push(returnType)
-
-              returnType = tsCodegen.namedType(returnType.name)
-            } else {
-              returnType = tsCodegen.namedType(
-                typesCodegen.ascTypeForEthereum(
+                    .map(coercion => coercion.toString())
+                    .join(', ')
+                : ''
+            }]
+          );
+          return ${
+            simpleReturnType
+              ? typesCodegen.ethereumValueToAsc(
+                  'result[0]',
                   member
                     .get('outputs')
                     .get(0)
                     .get('type'),
-                ),
-              )
-            }
-
-            // Generate and add a method that implements calling the function on
-            // the smart contract
-            klass.addMethod(
-              tsCodegen.method(
-                methodName,
-                member
-                  .get('inputs')
-                  .map((input, index) =>
-                    tsCodegen.param(
-                      paramName(input.get('name'), index),
-                      typesCodegen.ascTypeForEthereum(input.get('type')),
-                    ),
-                  ),
-                returnType,
-                `
-                let result = super.call(
-                  '${member.get('name')}',
-                  [${
-                    member.get('inputs')
-                      ? member
-                          .get('inputs')
-                          .map((input, index) =>
-                            typesCodegen.ethereumValueFromAsc(
-                              paramName(input.get('name'), index),
-                              input.get('type'),
-                            ),
-                          )
-                          .map(coercion => coercion.toString())
-                          .join(', ')
-                      : ''
-                  }]
-                );
-                return ${
-                  simpleReturnType
-                    ? typesCodegen.ethereumValueToAsc(
-                        'result[0]',
-                        member
-                          .get('outputs')
-                          .get(0)
-                          .get('type'),
-                      )
-                    : `new ${returnType.name}(
+                )
+              : `new ${returnType.name}(
                   ${member
                     .get('outputs')
                     .map((output, index) =>
@@ -452,12 +484,10 @@ module.exports = class AbiCodeGenerator {
                     )
                     .join(', ')}
                 )`
-                };
-                `,
-              ),
-            )
-          }
-      }
+          };
+          `,
+        ),
+      )
     })
 
     return [...types, klass]
