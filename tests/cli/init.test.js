f08e0da2d7db32b5f568a2d275558e87ba227c59
Jannis Pohlmann <jannis@thegraph.com>
2019-05-08 21:32:36 +0200

    tests/cli: Add init tests for contracts with tuples and overloading

--- tests/cli/init.test.js
+++ tests/cli/init.test.js
@@ -8,6 +8,8 @@ describe('Init', () => {
   let subgraphDir1 = path.join(baseDir, 'from-example')
   let subgraphDir2 = path.join(baseDir, 'from-contract')
   let subgraphDir3 = path.join(baseDir, 'from-contract-with-abi')
+  let subgraphDir4 = path.join(baseDir, 'from-contract-with-abi-and-structs')
+  let subgraphDir5 = path.join(baseDir, 'from-contract-with-overloaded-elements')
 
   const removeSubgraphDirs = () => {
     if (fs.existsSync(subgraphDir1)) {
@@ -18,6 +20,12 @@ describe('Init', () => {
     }
     if (fs.existsSync(subgraphDir3)) {
       fs.removeSync(subgraphDir3)
+    }
+    if (fs.existsSync(subgraphDir4)) {
+      fs.removeSync(subgraphDir4)
+    }
+    if (fs.existsSync(subgraphDir5)) {
+      fs.removeSync(subgraphDir5)
     }
   }
 
@@ -68,6 +76,40 @@ describe('Init', () => {
       subgraphDir3,
     ],
     'init/from-contract-with-abi',
+    { exitCode: 0, timeout: 60000, cwd: baseDir },
+  )
+
+  cliTest(
+    'From contract with abi and structs',
+    [
+      'init',
+      '--from-contract',
+      '0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e',
+      '--abi',
+      path.join(baseDir, 'abis', 'SoloMargin.json'),
+      '--network',
+      'mainnet',
+      'user/subgraph-from-contract-with-abi-and-structs',
+      subgraphDir4,
+    ],
+    'init/from-contract-with-abi-and-structs',
+    { exitCode: 0, timeout: 60000, cwd: baseDir },
+  )
+
+  cliTest(
+    'From contract with overloaded elements',
+    [
+      'init',
+      '--from-contract',
+      '0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e',
+      '--abi',
+      path.join(baseDir, 'abis', 'OverloadedElements.json'),
+      '--network',
+      'mainnet',
+      'user/subgraph-from-contract-with-overloaded-elements',
+      subgraphDir5,
+    ],
+    'init/from-contract-with-overloaded-elements',
     { exitCode: 0, timeout: 60000, cwd: baseDir },
   )
 })
