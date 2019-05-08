62325939af2c2f2797b8324fe8775d6b4782f73a
Jannis Pohlmann <jannis@thegraph.com>
2019-05-08 21:12:31 +0200

    scaffold: Disambiguate names, unroll tuples into several fields
    This adds the same disambiguation of event names and event input names as
    is used in code generation. It also unrolls tuples into multiple fields in
    the schema and field assignments in the generated mappings, thereby adding
    tuple support to `graph init` without requiring us to support inline objects
    just yet.

---
+++ src/scaffold.test.js
@@ -0,0 +1,145 @@
+const {
+  generateEventFieldAssignments,
+  generateManifest,
+  generateMapping,
+  generateSchema,
+} = require('./scaffold')
+
+const TEST_EVENT = {
+  name: 'ExampleEvent',
+  type: 'event',
+  inputs: [
+    { name: 'a', type: 'uint256' },
+    { name: 'b', type: 'bytes[4]' },
+    { type: 'string' },
+    {
+      name: 'c',
+      type: 'tuple',
+      components: [
+        { name: 'c1', type: 'uint256' },
+        { type: 'bytes32' },
+        { type: 'string' },
+        {
+          name: 'c3',
+          type: 'tuple',
+          components: [
+            { name: 'c31', type: 'uint96' },
+            { type: 'string' },
+            { type: 'bytes32' },
+          ],
+        },
+      ],
+    },
+    { name: 'd', type: 'string', indexed: true },
+  ],
+}
+
+const OVERLOADED_EVENT = {
+  name: 'ExampleEvent',
+  type: 'event',
+  inputs: [{ name: 'a', type: 'bytes32' }],
+}
+
+const TEST_CONTRACT = {
+  name: 'ExampleContract',
+  type: 'contract',
+}
+
+const TEST_ABI = [TEST_EVENT, OVERLOADED_EVENT, TEST_CONTRACT]
+
+describe('Subgraph scaffolding', () => {
+  test('Manifest', () => {
+    expect(
+      generateManifest({
+        abi: TEST_ABI,
+        network: 'kovan',
+        address: '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d',
+      }),
+    ).toEqual(`\
+specVersion: 0.0.1
+schema:
+  file: ./schema.graphql
+dataSources:
+  - kind: ethereum/contract
+    name: Contract
+    network: kovan
+    source:
+      address: "0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d"
+      abi: Contract
+    mapping:
+      kind: ethereum/events
+      apiVersion: 0.0.2
+      language: wasm/assemblyscript
+      entities:
+        - ExampleEvent
+        - ExampleEvent1
+      abis:
+        - name: Contract
+          file: ./abis/Contract.json
+      eventHandlers:
+        - event: ExampleEvent(uint256,bytes[4],string,tuple,string)
+          handler: handleExampleEvent
+        - event: ExampleEvent(bytes32)
+          handler: handleExampleEvent1
+      file: ./src/mapping.ts
+`)
+  })
+
+  test('Schema', () => {
+    expect(generateSchema({ abi: TEST_ABI })).toEqual(`\
+type ExampleEvent @entity {
+  id: ID!
+  a: BigInt! # uint256
+  b: [Bytes]! # bytes[4]
+  param2: String! # string
+  c_c1: BigInt! # uint256
+  c_value1: Bytes! # bytes32
+  c_value2: String! # string
+  c_c3_c31: BigInt! # uint96
+  c_c3_value1: String! # string
+  c_c3_value2: Bytes! # bytes32
+  d: String! # string
+}
+
+type ExampleEvent1 @entity {
+  id: ID!
+  a: Bytes! # bytes32
+}
+`)
+  })
+
+  test('Mapping', () => {
+    expect(generateMapping({ abi: TEST_ABI })).toEqual(`\
+import {
+  ExampleEvent as ExampleEventEvent,
+  ExampleEvent1 as ExampleEvent1Event
+} from "../generated/Contract/Contract"
+import { ExampleEvent, ExampleEvent1 } from "../generated/schema"
+
+export function handleExampleEvent(event: ExampleEventEvent): void {
+  let entity = new ExampleEvent(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.a = event.params.a
+  entity.b = event.params.b
+  entity.param2 = event.params.param2
+  entity.c_c1 = event.params.c.c1
+  entity.c_value1 = event.params.c.value1
+  entity.c_value2 = event.params.c.value2
+  entity.c_c3_c31 = event.params.c.c3.c31
+  entity.c_c3_value1 = event.params.c.c3.value1
+  entity.c_c3_value2 = event.params.c.c3.value2
+  entity.d = event.params.d
+  entity.save()
+}
+
+export function handleExampleEvent1(event: ExampleEvent1Event): void {
+  let entity = new ExampleEvent1(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.a = event.params.a
+  entity.save()
+}
+`)
+  })
+})
