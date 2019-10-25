29b70d80cc5d25589275e4f1e1abdb64a4c49f5c
Jorge Olivero <jorgeaolivero@gmail.com>
2019-10-25 14:17:46 -0500

    commands: Fix spacing

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -166,7 +166,7 @@ $ graph create --node ${node} ${subgraphName}`)
             process.exitCode = 1
           } else if (requestError) {
             spinner.fail(`HTTP error deploying the subgraph ${requestError.code}`)
-	    process.exitCode = 1
+            process.exitCode = 1
           } else {
             spinner.stop()
 
