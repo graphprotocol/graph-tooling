2f61de1366a2500df7dd1cb80902753baf7338c9
dOrgJelli <jelli@dorg.tech>
2020-03-03 00:12:58 -0500

    watcher: Mutations Support

--- src/watcher.js
+++ src/watcher.js
@@ -55,7 +55,7 @@ module.exports = class Watcher {
               files.push(path.resolve(path.join(dirname, filename)))
               return files
             }, files),
-          []
+          [],
         )
 
         let diff = (xs, ys) => ({
