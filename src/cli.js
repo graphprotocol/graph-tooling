d995c0a3ddf6a2971614e83496394fd7f414f499
Jannis Pohlmann <jannis@thegraph.com>
2020-08-18 02:47:26 +0200

    cli: Add support for CLI plugins

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,34 @@
-const { build } = require('gluegun')
+const path = require('path')
+const { exec } = require('child_process')
+const which = require('which')
+const { build, system } = require('gluegun')
 
 const run = async argv => {
-  const cli = build()
+  let cli = build()
     .brand('graph')
     .src(__dirname)
-    .plugins('./node_modules', { matching: 'graph-cli-*', hidden: true })
+
+  const pluginDirs = []
+  try {
+    const npmGlobalRoot = await system.run('npm root -g', { trim: true })
+    pluginDirs.push(npmGlobalRoot)
+  } catch (_) {}
+  try {
+    const npmUserRoot = await system.run('npm root', { trim: true })
+    pluginDirs.push(npmUserRoot)
+  } catch (_) {}
+  try {
+    const yarnUserRoot = await system.run('yarn global dir', { trim: true })
+    pluginDirs.push(yarnUserRoot)
+  } catch (_) {}
+
+  // Inject potential plugin directories
+  cli = pluginDirs.reduce(
+    (cli, dir) => cli.plugin(path.join(dir, '@graphprotocol', 'indexer-cli', 'dist')),
+    cli,
+  )
+
+  cli = cli
     .help()
     .version()
     .defaultCommand()
