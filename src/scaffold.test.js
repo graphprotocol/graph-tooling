68ea82d7114f27bdb5414cc864a55f56c7968e26
Lauren Garcia <lauren.garcia@gmail.com>
2020-12-21 19:56:25 -0800

    scaffold.js: load() transaction hash as unique id

--- src/scaffold.test.js
+++ src/scaffold.test.js
@@ -159,12 +159,12 @@ import { ExampleEntity } from "../generated/schema"
 export function handleExampleEvent(event: ExampleEvent): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = ExampleEntity.load(event.transaction.hash.toHex())
 
   // Entities only exist after they have been saved to the store;
   // \`null\` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new ExampleEntity(event.transaction.hash.toHex())
 
     // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0)
