9b6fde9cc77842116cb1aebec2d5317813890354
Martynas Prokopas <promaty@gmail.com>
2021-07-03 02:08:44 +0200

    remove keytar to support linux & windows

--- src/command-helpers/auth.js
+++ src/command-helpers/auth.js
@@ -1,25 +1,20 @@
 const toolbox = require('gluegun/toolbox')
 const { normalizeNodeUrl } = require('./node')
 
-// keytar is listed as an optional dependency in package.json and
-// dynamically required in this function
-const requireKeytar = () => {
+const fs = require('fs')
+const path = require('path')
+const homedir = require('os').homedir()
+
+const CONFIG_PATH = path.join(homedir, '/.graph-cli.json')
+const getConfig = () => {
+  let config
   try {
-    return require('keytar')
-  } catch (e) {
-    throw new Error(
-      `
-
-Cannot store the deploy key because dependencies are missing. If you
-are on Linux, try installing 'libsecret-1-dev' (Debian, Ubuntu etc.) or
-'libsecret-devel' (RedHat, Fedora etc.) and reinstalling Graph CLI
-afterwards.
-
-The original error was: ${e.message}
-      `,
-    )
+    config = JSON.parse(fs.readFileSync(CONFIG_PATH))
+  } catch {
+    config = {}
   }
-}
+  return config
+ }
 
 const identifyDeployKey = async (node, deployKey) => {
   // Determine the deploy key to use, if any:
@@ -29,28 +24,11 @@ const identifyDeployKey = async (node, deployKey) => {
     return deployKey
   } else {
     try {
-      const keytar = requireKeytar()
       node = normalizeNodeUrl(node)
-      return await keytar.getPassword('graphprotocol-auth', node)
+      const config = getConfig()
+      return config[node]
     } catch (e) {
-      if (process.platform === 'win32') {
-        toolbox.print.warning(
-          `Could not get deploy key from Windows Credential Vault: ${e.message}`,
-        )
-      } else if (process.platform === 'darwin') {
-        toolbox.print.warning(
-          `Could not get deploy key from macOS Keychain: ${e.message}`,
-        )
-      } else if (process.platform === 'linux') {
-        toolbox.print.warning(
-          `Could not get deploy key from libsecret ` +
-            `(usually gnome-keyring or ksecretservice): ${e.message}`,
-        )
-      } else {
-        toolbox.print.warning(
-          `Could not get deploy key from OS secret storage service: ${e.message}`,
-        )
-      }
+      toolbox.print.warning(`Could not get deploy key: ${e.message}`)
       toolbox.print.info(`Continuing without an deploy key\n`)
     }
   }
@@ -58,22 +36,12 @@ const identifyDeployKey = async (node, deployKey) => {
 
 const saveDeployKey = async (node, deployKey) => {
   try {
-    const keytar = requireKeytar()
     node = normalizeNodeUrl(node)
-    await keytar.setPassword('graphprotocol-auth', node, deployKey)
+    const config = getConfig()
+    config[node] = deployKey
+    fs.writeFileSync(CONFIG_PATH, JSON.stringify(config))
   } catch (e) {
-    if (process.platform === 'win32') {
-      throw new Error(`Error storing deploy key in Windows Credential Vault: ${e}`)
-    } else if (process.platform === 'darwin') {
-      throw new Error(`Error storing deploy key in macOS Keychain: ${e}`)
-    } else if (process.platform === 'linux') {
-      throw new Error(
-        `Error storing deploy key with libsecret ` +
-          `(usually gnome-keyring or ksecretservice): ${e}`,
-      )
-    } else {
-      throw new Error(`Error storing deploy key in OS secret storage service: ${e}`)
-    }
+    throw new Error(`Error storing deploy key: ${e.message}`)
   }
 }
 
