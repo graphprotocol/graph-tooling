36cc35981139f9e9c05bbbd6973b5c16759e1dd1
Otávio Pace <otaviopp8@gmail.com>
2021-08-07 12:45:37 -0300

    chore: Change AssemblyScript migration guide link to gist temporarly (alpha release)

--- src/command-helpers/version.test.js
+++ src/command-helpers/version.test.js
@@ -205,7 +205,7 @@ describe('Version Command Helpers', () => {
         .rejects
         .toThrow(new Error(
           `To use this version of the graph-cli you must upgrade the graph-ts dependency to a version greater than or equal to ${minimumGraphTsVersion}
-Also, you'll probably need to take a look at our AssemblyScript migration guide because of language breaking changes: https://thegraph.com/docs/assemblyscript-migration-guide`
+Also, you'll probably need to take a look at our AssemblyScript migration guide because of language breaking changes: https://gist.github.com/otaviopace/8406cb39644d2e7678570d1e7f50dac4`
         ))
     })
     test('When the installed graph-ts version is a supported one', async () => {
