135679e524a39d0a4e489fa9243ede1dde0130dd
georg-getz <georgi.getz@outlook.com>
2021-08-26 13:41:43 +0300

    added options for version and force

--- src/commands/test.js
+++ src/commands/test.js
@@ -2,27 +2,62 @@ const tmp = require('tmp-promise')
 const { Binary } = require('binary-install-raw')
 const { XMLHttpRequest } = require('xmlhttprequest')
 const os = require('os')
+const chalk = require('chalk')
 
 // Clean up temporary files even when an uncaught exception occurs
 tmp.setGracefulCleanup()
 
+const HELP = `
+${chalk.bold('graph test')} ${chalk.dim('[options]')} ${chalk.bold('<datasource>')}
+
+${chalk.dim('Options:')}
+
+  -f  --force                   Overwrite folder + file when downloading
+  -h, --help                    Show usage information
+  -v, --version <tag>           Choose the version of the rust binary that you want to be downloaded/used
+  `
+
 module.exports = {
   description: 'Runs rust binary for subgraph testing',
   run: async toolbox => {
-    console.log(toolbox);
+    // Obtain tools
+    let { print } = toolbox
+
+    // Read CLI parameters
+    let { f, force, h, help, v, version } = toolbox.parameters.options
+    let datasource = toolbox.parameters.first
+
+    // Support both long and short option variants
+    force = force || f
+    help = help || h
+    version = version || v
+
+    console.log(force);
+    console.log(help);
+    console.log(version);
+
+    // Show help text if requested
+    if (help) {
+      print.info(HELP)
+      return
+    }
+
+    // console.log(toolbox);
     const platform = getPlatform();
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', 'https://api.github.com/repos/LimeChain/matchstick/releases/latest', false);
-    xmlHttp.send();
-    const jsonResponse = JSON.parse(xmlHttp.responseText);
-    const version = jsonResponse.tag_name;
+    if (!version) {
+      let xmlHttp = new XMLHttpRequest();
+      xmlHttp.open('GET', 'https://api.github.com/repos/LimeChain/matchstick/releases/latest', false);
+      xmlHttp.send();
+      const jsonResponse = JSON.parse(xmlHttp.responseText);
+      version = jsonResponse.tag_name;
+    }
+
     const url = `https://github.com/LimeChain/matchstick/releases/download/${ version }/${ platform }`;
-    const name = 'matchstick';
     console.log(version)
 
     let binary = new Binary(platform, url, version);
-    await binary.install();
-    binary.run(toolbox.parameters.first);
+    await binary.install(force);
+    binary.run(datasource);
 
     return binary;
   }
