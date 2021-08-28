fa36a1270332eb7e81e8e0ccb3b023d4b2ba5ca9
Otávio Pace <otaviopp8@gmail.com>
2021-08-28 09:17:02 -0300

    lint: Add script with new eslintrc config file

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        node: true,
+        es2017: true,
+        jest: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+    },
+}
