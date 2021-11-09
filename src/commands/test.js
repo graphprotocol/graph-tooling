e70649ba2ff0c70d1eef9caac9407413578369d2
georg-getz <georgi.getz@outlook.com>
2021-11-09 13:35:44 +0200

    added a bonus flag for listing important logs

--- src/commands/test.js
+++ src/commands/test.js
@@ -11,6 +11,7 @@ ${chalk.dim('Options:')}
 
   -f  --force                   Overwrite folder + file when downloading
   -h, --help                    Show usage information
+  -l, --logs                    Logs to the console the value of almost every single variable
   -v, --version <tag>           Choose the version of the rust binary that you want to be downloaded/used
   `
 
@@ -21,12 +22,13 @@ module.exports = {
     let { print } = toolbox
 
     // Read CLI parameters
-    let { f, force, h, help, v, version } = toolbox.parameters.options
+    let { f, force, h, help, l, logs, v, version } = toolbox.parameters.options
     let datasource = toolbox.parameters.first
 
     // Support both long and short option variants
     force = force || f
     help = help || h
+    logs = logs || l
     version = version || v
 
     // Show help text if requested
@@ -35,53 +37,56 @@ module.exports = {
       return
     }
 
-    const platform = getPlatform();
+    const platform = getPlatform(logs)
     if (!version) {
-      let result = await fetch('https://api.github.com/repos/LimeChain/matchstick/releases/latest');
-      let json = await result.json();
-      version = json.tag_name;
+      let result = await fetch('https://api.github.com/repos/LimeChain/matchstick/releases/latest')
+      let json = await result.json()
+      version = json.tag_name
     }
 
-    const url = `https://github.com/LimeChain/matchstick/releases/download/${version}/${platform}`;
+    const url = `https://github.com/LimeChain/matchstick/releases/download/${version}/${platform}`
 
-    let binary = new Binary(platform, url, version);
-    await binary.install(force);
-    datasource ? binary.run(datasource) : binary.run();
+    if (logs) {
+      console.log(`Download link: ${url}`)
+    }
+
+    let binary = new Binary(platform, url, version)
+    await binary.install(force)
+    datasource ? binary.run(datasource) : binary.run()
   }
 }
 
-function getPlatform() {
-  const type = os.type();
-  const arch = os.arch();
-  const release = os.release();
-  const cpuCore = os.cpus()[0];
-  const majorVersion = semver.major(release);
-  const isM1 = cpuCore.model.includes("Apple M1");
+function getPlatform(logs) {
+  const type = os.type()
+  const arch = os.arch()
+  const release = os.release()
+  const cpuCore = os.cpus()[0]
+  const majorVersion = semver.major(release)
+  const isM1 = cpuCore.model.includes("Apple M1")
 
-  console.log("os type: " + type)
-  console.log("os arch: " + arch)
-  console.log("os release: " + release)
-  console.log("os major version: " + majorVersion)
+  if (logs) {
+    console.log(`OS type: ${type}\nOS arch: ${arch}\nOS release: ${release}\nOS major version: ${majorVersion}\nCPU model: ${cpuCore.model}`)
+  }
 
   if (arch === 'x64' || (arch === 'arm64' && isM1)) {
     if (type === 'Darwin') {
       if (majorVersion === 19) {
-        return 'binary-macos-10.15';
+        return 'binary-macos-10.15'
       } else if (majorVersion === 18) {
-        return 'binary-macos-10.14';
+        return 'binary-macos-10.14'
       } else if (isM1) {
-        return 'binary-macos-11-m1';
+        return 'binary-macos-11-m1'
       }
-      return 'binary-macos-11';
+      return 'binary-macos-11'
     } else if (type === 'Linux') {
       if (majorVersion === 18) {
-        return 'binary-linux-18';
+        return 'binary-linux-18'
       }
-      return 'binary-linux-20';
+      return 'binary-linux-20'
     } else if (type === 'Windows_NT') {
-      return 'binary-windows';
+      return 'binary-windows'
     }
   }
 
-  throw new Error(`Unsupported platform: ${type} ${arch} ${majorVersion}`);
+  throw new Error(`Unsupported platform: ${type} ${arch} ${majorVersion}`)
 }
