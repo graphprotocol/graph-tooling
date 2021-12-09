ad8c5b05fae64f7047811ca3f8100cf2f1513acb
Petko <pavlovskipetko@abv.bg>
2021-12-09 15:45:37 +0200

    Add Docker support

--- src/commands/test.js
+++ src/commands/test.js
@@ -2,6 +2,8 @@ const { Binary } = require('binary-install-raw')
 const os = require('os')
 const chalk = require('chalk')
 const fetch = require('node-fetch')
+const { exec } = require('child_process')
+const fs = require('fs')
 const semver = require('semver')
 
 const HELP = `
@@ -13,6 +15,7 @@ ${chalk.dim('Options:')}
   -h, --help                    Show usage information
   -l, --logs                    Logs to the console information about the OS, CPU model and download url (debugging purposes)
   -v, --version <tag>           Choose the version of the rust binary that you want to be downloaded/used
+  -d, --docker                  Launches Matchstick in a docker container
   `
 
 module.exports = {
@@ -22,7 +25,7 @@ module.exports = {
     let { print } = toolbox
 
     // Read CLI parameters
-    let { f, force, h, help, l, logs, v, version } = toolbox.parameters.options
+    let { f, force, h, help, l, logs, v, version, d, docker } = toolbox.parameters.options
     let datasource = toolbox.parameters.first
 
     // Support both long and short option variants
@@ -34,6 +37,101 @@ module.exports = {
     // Show help text if requested
     if (help) {
       print.info(HELP)
+      return
+    }
+
+    if (docker) {
+      let url = `https://github.com/LimeChain/matchstick/releases/download/${version || '0.2.0a'}/binary-linux-20`
+
+      if (version) {
+       await fetch(url)
+          .then(response => {
+            if (response.status === 404) {
+              print.info(`Error: Invalid Matchstick version '${version}'`)
+              process.exit(1)
+            }
+          })
+      }
+
+      // TODO: Move these in separate file (in a function maybe)
+      let contents = `FROM ubuntu:20.04
+ENV ARGS=""
+RUN apt update
+RUN apt install -y nodejs
+RUN apt install -y npm
+RUN apt install -y cmake
+RUN apt install -y git
+COPY ./ ./
+RUN mkdir ./tests/.tools
+WORKDIR ./tests/.tools
+RUN git clone --recursive https://github.com/WebAssembly/wabt
+WORKDIR wabt
+RUN git submodule update --init
+RUN mkdir build
+WORKDIR build
+RUN cmake ..
+RUN cmake --build .
+WORKDIR /
+RUN npm run codegen
+RUN npm run build
+RUN apt install -y postgresql
+RUN apt install -y curl
+RUN curl -OL ${url}
+RUN mv binary-linux-20 matchstick
+CMD ./matchstick \${ARGS}`
+
+      let dir = 'tests/.docker'
+
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true })
+      }
+
+      fs.writeFileSync('tests/.docker/Dockerfile', contents, (err) => {
+        if (err) {
+          print.info('A problem occurred while generating the Dockerfile. Please attend to the errors below.')
+          print.info(err)
+        } else {
+          print.info('Successfully generated Dockerfile.')
+        }
+      })
+
+      exec(`docker build -f tests/.docker/Dockerfile -t matchstick . `, (error, stdout, stderr) => {
+        print.info('Building Matchstick image...')
+
+        if (error) {
+          print.info('A problem occurred while trying to build the Matchstick Docker image. Please attend to the errors below.')
+          print.info(`error: ${error.message}`)
+        }
+        if (stderr) {
+          print.info('A problem occurred while trying to build the Matchstick Docker image. Please attend to the errors below.')
+          print.info(`stderr: ${stderr}`)
+        }
+
+        let runCommand = `docker run -e ARGS="${datasource || ''}${coverage ? '-c' : ''}" --rm matchstick`
+
+        console.log('runCommand output')
+        console.log(runCommand)
+        print.info('runCommand output')
+        print.info(runCommand)
+
+        exec(runCommand, (error, stdout, stderr) => {
+          print.info('Running Matchstick image...')
+
+          if (error) {
+            print.info('A problem occurred while trying to run the Matchstick Docker image. Please attend to the errors below.')
+            print.info(`error: ${error.message}`)
+            process.exit(1)
+          }
+          if (stderr) {
+            print.info('A problem occurred while trying to run the Matchstick Docker image. Please attend to the errors below.')
+            print.info(`stderr: ${stderr}`)
+            process.exit(1)
+          }
+          print.info(stdout)
+          process.exit()
+        })
+      })
+
       return
     }
 
@@ -53,7 +151,7 @@ module.exports = {
     let binary = new Binary(platform, url, version)
     await binary.install(force)
     datasource ? binary.run(datasource) : binary.run()
-  }
+  },
 }
 
 function getPlatform(logs) {
@@ -62,7 +160,7 @@ function getPlatform(logs) {
   const release = os.release()
   const cpuCore = os.cpus()[0]
   const majorVersion = semver.major(release)
-  const isM1 = cpuCore.model.includes("Apple M1")
+  const isM1 = cpuCore.model.includes('Apple M1')
 
   if (logs) {
     console.log(`OS type: ${type}\nOS arch: ${arch}\nOS release: ${release}\nOS major version: ${majorVersion}\nCPU model: ${cpuCore.model}`)
