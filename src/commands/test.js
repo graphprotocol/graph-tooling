cc6e76b7d74b86394c1af4aa08834a14801850b1
Petko <pavlovskipetko@abv.bg>
2021-12-13 15:10:25 +0200

    Update

--- src/commands/test.js
+++ src/commands/test.js
@@ -48,21 +48,38 @@ module.exports = {
     }
 
     // TODO: Move these in separate file (in a function maybe)
-    let contents = `FROM ubuntu:20.04
+    let contents = `# I'll try to run it on alpine
+FROM ubuntu:20.04
+
+# Not sure if this ENV declaration is necessary
 ENV ARGS=""
+
+# Install necessary packages
 RUN apt update
 RUN apt install -y nodejs
 RUN apt install -y npm
-COPY ./ ./
-RUN npm run codegen
-RUN npm run build
+RUN apt install -y git
 RUN apt install -y postgresql
 RUN apt install -y curl
-RUN apt-get update && apt-get -y install cmake protobuf-compiler
-RUN curl -OL https://github.com/LimeChain/matchstick/releases/download/${version || "0.2.2a"}/binary-linux-20
-RUN mv binary-linux-20 matchstick
-RUN chmod a+x matchstick
-CMD ./matchstick \${ARGS}`
+RUN npm install -g @graphprotocol/graph-cli
+
+# Download the latest linux binary
+RUN curl -OL https://github.com/LimeChain/matchstick/releases/download/0.2.2a/binary-linux-20
+# Make it executable
+RUN chmod a+x binary-linux-20
+
+# Create a matchstick dir where the host will be copied
+RUN mkdir matchstick
+WORKDIR matchstick
+
+# Copy host to /matchstick
+COPY ../ .
+
+RUN graph codegen
+RUN graph build
+
+CMD ../binary-linux-20 \${ARGS}
+`
 
     let dir = 'tests/.docker';
 
