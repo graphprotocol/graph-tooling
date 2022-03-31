c9169b1ef2b91c76fc6ec1e895f5642227b4a12a
tilacog <tilacog@gmail.com>
2022-03-31 12:41:13 -0300

    Bump `graph-ts` version to 0.27.0-alpha.0

--- src/scaffold/index.js
+++ src/scaffold/index.js
@@ -50,7 +50,7 @@ module.exports = class Scaffold {
         },
         dependencies: {
           '@graphprotocol/graph-cli': GRAPH_CLI_VERSION,
-          '@graphprotocol/graph-ts': `0.27.0`,
+          '@graphprotocol/graph-ts': `0.27.0-alpha.0`,
         },
       }),
       { parser: 'json' },
