d9d184b9887987aacfba6fe344f95e3974963d9d
clearloop <26088946+clearloop@users.noreply.github.com>
2022-04-21 22:19:35 +0800

    feat(protocol): update scaffold of arweave

--- src/protocols/arweave/scaffold/mapping.js
+++ src/protocols/arweave/scaffold/mapping.js
@@ -1,27 +1,21 @@
 const generatePlaceholderHandlers = () =>
   `
-  import { near, BigInt } from '@graphprotocol/graph-ts'
-  import { ExampleEntity } from '../generated/schema'
+  import { arweave, BigInt } from '@graphprotocol/graph-ts'
+  import { BlockEvent } from '../generated/schema'
 
-  export function handleReceipt(receiptWithOutcome: near.ReceiptWithOutcome): void {
-    // Entities can be loaded from the store using a string ID; this ID
+  export function handleReceipt(block: arweave.Block): void {
+    // Entities can be loaded from the store using a indepHash; this indepHash
     // needs to be unique across all entities of the same type
-    let entity = ExampleEntity.load(receiptWithOutcome.receipt.id.toHex())
+    let entity = BlockEvent.load(block.indepHash.toHexString())
 
     // Entities only exist after they have been saved to the store;
     // \`null\` checks allow to create entities on demand
     if (!entity) {
-      entity = new ExampleEntity(receiptWithOutcome.receipt.id.toHex())
+      entity = new BlockEvent(block.indepHash.toHexString());
 
       // Entity fields can be set using simple assignments
-      entity.count = BigInt.fromI32(0)
+      entity.height = BigInt.fromU64(block.height)
     }
-
-    // BigInt and BigDecimal math are supported
-    entity.count = entity.count + BigInt.fromI32(1)
-
-    // Entity fields can be set based on receipt information
-    entity.block = receiptWithOutcome.block.header.hash
 
     // Entities can be written to the store with \`.save()\`
     entity.save()
