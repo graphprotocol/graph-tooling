955fcfd77dafae8ea5d034046893575e811391c8
David Lutterkort <lutter@watzmann.net>
2022-05-18 11:55:30 +0200

    Handle interfaces with Bytes id correctly in codegen
    Fixes https://github.com/graphprotocol/graph-ts/issues/278

--- src/codegen/schema.test.js
+++ src/codegen/schema.test.js
@@ -355,4 +355,106 @@ describe('Schema code generator', () => {
       })
     })
   })
+
+  test('Should handle references with Bytes id types', () => {
+    const codegen = createSchemaCodeGen(`
+    interface Employee {
+      id: Bytes!
+      name: String!
+     }
+
+    type Worker implements Employee @entity {
+      id: Bytes!
+      name: String!
+   }
+
+    type Task @entity {
+      id: Bytes!
+      employee: Employee!
+      worker: Worker!
+   }
+`)
+
+    const generatedTypes = codegen.generateTypes()
+    testEntity(generatedTypes, {
+      name: "Task",
+      members: [],
+      methods: [
+        {
+          name: 'constructor',
+          params: [new Param('id', new NamedType('Bytes'))],
+          returnType: undefined,
+          body: "\n      super()\n      this.set('id', Value.fromBytes(id))\n      "
+        },
+        {
+          name: 'save',
+          params: [],
+          returnType: new NamedType('void'),
+          body: '\n' +
+            "        let id = this.get('id')\n" +
+            '        assert(id != null,\n' +
+            "               'Cannot save Task entity without an ID')\n" +
+            '        if (id) {\n' +
+            '          assert(id.kind == ValueKind.BYTES,\n' +
+            "                 `Entities of type Task must have an ID of type Bytes but the id '${id.displayData()}' is of type ${id.displayKind()}`)\n" +
+            "          store.set('Task', id.toBytes().toHexString(), this)\n" +
+            '        }'
+        },
+        {
+          name: 'load',
+          static: true,
+          params: [new Param('id', new NamedType('Bytes'))],
+          returnType: new NullableType(new NamedType('Task')),
+          body: '\n' +
+            "        return changetype<Task | null>(store.get('Task', id.toHexString()))\n" +
+            '        '
+        },
+        {
+          name: 'get id',
+          params: [],
+          returnType: new NamedType('Bytes'),
+          body: '\n' +
+            "       let value = this.get('id')\n" +
+            '       return value!.toBytes()\n' +
+            '      '
+        },
+        {
+          name: 'set id',
+          params: [new Param('value', new NamedType('Bytes'))],
+          returnType: undefined,
+          body: "\n      this.set('id', Value.fromBytes(value))\n    "
+        },
+        {
+          name: 'get employee',
+          params: [],
+          returnType: new NamedType('Bytes'),
+          body: '\n' +
+            "       let value = this.get('employee')\n" +
+            '       return value!.toBytes()\n' +
+            '      '
+        },
+        {
+          name: 'set employee',
+          params: [new Param('value', new NamedType('Bytes'))],
+          returnType: undefined,
+          body: "\n      this.set('employee', Value.fromBytes(value))\n    "
+        },
+        {
+          name: 'get worker',
+          params: [],
+          returnType: new NamedType('Bytes'),
+          body: '\n' +
+            "       let value = this.get('worker')\n" +
+            '       return value!.toBytes()\n' +
+            '      '
+        },
+        {
+          name: 'set worker',
+          params: [new Param('value', new NamedType('Bytes'))],
+          returnType: undefined,
+          body: "\n      this.set('worker', Value.fromBytes(value))\n    "
+        }
+      ]
+    })
+  })
 })
