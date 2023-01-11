312dd021fa7d770d0c98db0aa346a50f7d2a42ce
enisdenjo <badurinadenis@gmail.com>
2023-01-11 12:48:01 +0100

    migration

--- packages/cli/src/validation/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  validateSchema: require('./schema').validateSchema,
-  validateManifest: require('./manifest').validateManifest,
-  validateContractValues: require('./contract').validateContractValues,
-  validateContract: require('./contract').validateContract,
-}
---
+++ packages/cli/src/validation/index.ts
@@ -0,0 +1,10 @@
+import { validateSchema } from './schema'
+import { validateManifest } from './manifest'
+import { validateContractValues, validateContract } from './contract'
+
+export default {
+  validateSchema,
+  validateManifest,
+  validateContractValues,
+  validateContract,
+}
