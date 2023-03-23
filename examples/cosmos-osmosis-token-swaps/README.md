ff8a046ff04ecce6bbc23edf2a0ef2a30580ac05
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2023-03-23 16:07:04 -0400

    move cosmos/osmosis-token-swaps

---
+++ examples/cosmos-osmosis-token-swaps/README.md
@@ -0,0 +1,26 @@
+# Osmosis Token Swaps Example
+
+This example subgraph stores `TokenSwap` objects that represents token swaps made using the [GAMM](https://docs.osmosis.zone/developing/osmosis-core/modules/spec-gamm.html) (Generalized Automated Market Maker) in the Osmosis chain. In order to do that, an event handler is used to filter [token_swapped](https://github.com/osmosis-labs/osmosis/blob/c8ac95c6a6ea42e934d49599eafc8609b3c6fe61/x/gamm/types/events.go#L13) events. The type of event to be filtered is specified in the subgraph manifest file. That way, the handler will just receive events of that type.
+
+By running this example subgraph, and with the following query, you can retrieve all the swaps made by a given address:
+
+```
+query SwapsForAccount($senderAddress: String!) {
+  tokenSwaps(where: {sender: $senderAddress}) {
+    tokenIn {
+      amount
+      denom
+    },
+    tokenOut {
+      amount
+      denom
+    }
+  }
+}
+```
+```
+{
+  "senderAddress": "osmo1wd3j7cvcnr3pfey4fx2mz9xml9euu68z6zg0xp"
+}
+```
+For more information see the docs on https://thegraph.com/docs/.
---
+++ examples/cosmos-osmosis-token-swaps/src/mapping.ts
@@ -0,0 +1,32 @@
+import { cosmos } from "@graphprotocol/graph-ts";
+import { TokenSwap, Token } from "../generated/schema";
+
+export function handleSwaps(data: cosmos.EventData): void {
+  const height = data.block.header.height;
+  const sender = data.event.getAttributeValue("sender");
+  const poolId = data.event.getAttributeValue("pool_id");
+
+  let swap = new TokenSwap(`${height}-${sender}`);
+  swap.sender = sender;
+  swap.poolId = poolId;
+  swap.tokenIn = saveToken(`${height}-${sender}-in`, data.event.getAttributeValue("tokens_in"));
+  swap.tokenOut = saveToken(`${height}-${sender}-out`, data.event.getAttributeValue("tokens_out"));
+
+  swap.save();
+}
+
+function saveToken(id: string, data: string): string {
+  const tokenIn = new Token(id);
+
+  // When assets are transferred through IBC, they lose their original denomination (i.e ATOM)
+  // and obtain a new IBC denomination (i.e. ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2).
+  // Check this page (https://docs.osmosis.zone/developing/assets/asset-info.html) for a full list of IBC denomination
+  // Both amount and denomination come together in the message value (i.e. 123456uatom), so we need to separate them.
+  let tokenDenomLength = data.includes('ibc') ? 68 : 5 // IBC denomination is 68 characters long, OSMO is 5 characters long.
+
+  tokenIn.denom = data.substring(data.length - tokenDenomLength, data.length);
+  tokenIn.amount = data.substring(0, data.length - tokenDenomLength);
+  tokenIn.save();
+
+  return id;
+}
