569bb80f387da90f8e054bc2f332e5b0de96a523
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2023-04-13 14:32:18 -0400

    fix: append /api/v0 to graph IPFS endpoint

---
+++ packages/cli/src/command-helpers/compiler.test.ts
@@ -0,0 +1,43 @@
+import { appendApiVersionForGraph } from './compiler';
+
+describe('appendApiVersionForGraph', () => {
+  it('append /api/v0 to Prod URL with trailing slash', () => {
+    expect(appendApiVersionForGraph('https://api.thegraph.com/ipfs/')).toBe(
+      'https://api.thegraph.com/ipfs/api/v0',
+    );
+  });
+
+  it('append /api/v0 to Prod URL without trailing slash', () => {
+    expect(appendApiVersionForGraph('https://api.thegraph.com/ipfs')).toBe(
+      'https://api.thegraph.com/ipfs/api/v0',
+    );
+  });
+
+  it('append /api/v0 to Staging URL without trailing slash', () => {
+    expect(appendApiVersionForGraph('https://staging.api.thegraph.com/ipfs')).toBe(
+      'https://staging.api.thegraph.com/ipfs/api/v0',
+    );
+  });
+
+  it('do nothing if Prod URL has /api/v0', () => {
+    expect(appendApiVersionForGraph('https://api.thegraph.com/ipfs/api/v0')).toBe(
+      'https://api.thegraph.com/ipfs/api/v0',
+    );
+  });
+
+  it('do nothing if Prod URL has no /ipfs', () => {
+    expect(appendApiVersionForGraph('https://api.thegraph.com')).toBe(
+      'https://api.thegraph.com',
+    );
+  });
+
+  it('do nothing for non-graph endpoint', () => {
+    expect(appendApiVersionForGraph('https://ipfs.saihaj.dev/')).toBe('https://ipfs.saihaj.dev/');
+  });
+
+  it('do nothing for non-graph endpoint ending with /ipfs', () => {
+    expect(appendApiVersionForGraph('https://ipfs.saihaj.dev/ipfs/')).toBe(
+      'https://ipfs.saihaj.dev/ipfs/',
+    );
+  });
+});
