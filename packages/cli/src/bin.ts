17b90d7e2d4abdd850755adc4630975fd2b957e7
Denis Badurina <badurinadenis@gmail.com>
2023-04-26 17:15:23 +0200

    Bundle CLI as binary (#1257)
    * update lockfile
    
    * ignore tmp
    
    * oclif pack
    
    * cli ci
    
    * install.sh
    
    * bump pnpm
    
    * patch oclif
    
    * upload artifact
    
    * ci adjustments
    
    * steps
    
    * rename tarballs
    
    * test
    
    * use bin run
    
    * use bin run in scripts
    
    * also bin run in cli tests
    
    * basename for renaming log
    
    * no cli in pr ci
    
    * changeset

--- packages/cli/src/bin.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import * as oclif from '@oclif/core';
-import flush from '@oclif/core/flush';
-// @ts-expect-error Handle is a pure JS file
-import handle from '@oclif/core/handle';
-
-oclif.run().then(flush).catch(handle);
