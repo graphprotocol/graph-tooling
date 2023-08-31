7d4900f1c867cc53b833d6e0f4ced2d94f4f9cb1
Filipe Azevedo <filipe@azevedo.io>
2023-08-31 09:59:02 +0100

    format

--- examples/matic-lens-protocol-posts-subgraph/src/contract.ts
+++ examples/matic-lens-protocol-posts-subgraph/src/contract.ts
@@ -1,42 +1,45 @@
-import { Bytes, dataSource, DataSourceContext, DataSourceTemplate, ipfs } from "@graphprotocol/graph-ts";
 import {
-  PostCreated as PostCreatedEvent,
-} from "../generated/Contract/Contract"
-import { PostContent, PostCreated } from "../generated/schema"
+  Bytes,
+  dataSource,
+  DataSourceContext,
+  DataSourceTemplate,
+  ipfs
+} from "@graphprotocol/graph-ts";
+import { PostCreated as PostCreatedEvent } from "../generated/Contract/Contract";
+import { PostContent, PostCreated } from "../generated/schema";
 
 const POST_ID_KEY = "postID";
 
 export function handlePostCreated(event: PostCreatedEvent): void {
   let entity = new PostCreated(
-    Bytes.fromUTF8(event.params.profileId.toString()+"-"+event.params.pubId.toString())
+    Bytes.fromUTF8(
+      event.params.profileId.toString() + "-" + event.params.pubId.toString()
+    )
   );
 
   entity.ownerId = event.params.profileId;
   entity.contentURI = event.params.contentURI;
   entity.timestamp = event.params.timestamp;
 
-  entity.save()
-
+  entity.save();
 
   let arweaveIndex = entity.contentURI.indexOf("arweave.net/");
   let ipfsIndex = entity.contentURI.indexOf("/ipfs/");
 
-  if (arweaveIndex == -1 && ipfsIndex == -1) 
-    return;
-  
+  if (arweaveIndex == -1 && ipfsIndex == -1) return;
 
   let context = new DataSourceContext();
   context.setBytes(POST_ID_KEY, entity.id);
 
   if (arweaveIndex != -1) {
-    let hash = entity.contentURI.substr(arweaveIndex+12);
+    let hash = entity.contentURI.substr(arweaveIndex + 12);
     DataSourceTemplate.createWithContext("ArweaveContent", [hash], context);
 
     return;
   }
 
   if (ipfsIndex != -1) {
-    let hash = entity.contentURI.substr(ipfsIndex+6);
+    let hash = entity.contentURI.substr(ipfsIndex + 6);
     DataSourceTemplate.createWithContext("IpfsContent", [hash], context);
   }
 }
@@ -45,9 +48,9 @@ export function handlePostContent(content: Bytes): void {
   let hash = dataSource.stringParam();
   let ctx = dataSource.context();
   let id = ctx.getBytes(POST_ID_KEY);
-  
+
   let post = new PostContent(id);
   post.hash = hash;
   post.content = content.toString();
-  post.save()
+  post.save();
 }
