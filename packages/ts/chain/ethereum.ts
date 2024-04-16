e299f6ce5cf1ad74cab993f6df3feb7ca9993254
Krishnanand V P <44740264+incrypto32@users.noreply.github.com>
2024-04-16 18:37:39 +0400

    Add support for ethereum.getCode (#1609)
    * Add `getCode` method to ethereum API
    
    * Add changeset

--- packages/ts/chain/ethereum.ts
+++ packages/ts/chain/ethereum.ts
@@ -6,6 +6,7 @@ import { Address, BigInt } from '../common/numbers';
 export declare namespace ethereum {
   function call(call: SmartContractCall): Array<Value> | null;
   function getBalance(address: Address): BigInt;
+  function hasCode(address: Address): bool;
   function encode(token: Value): Bytes | null;
   function decode(types: String, data: Bytes): Value | null;
 }
