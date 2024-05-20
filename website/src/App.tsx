07bc00d1926c113bcbd66d8270128c5c07169c8f
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-05-20 10:23:51 -0600

    setup shadcn

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen flex justify-center items-center">
       <a href="https://thegraph.com" target="_blank">
         <img src="the-graph-logomark-light.png" className="logo react" alt="The Graph logo" />
       </a>
--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './global.css';
+import { ThemeProvider } from './components/theme-provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="graph-tooling">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
