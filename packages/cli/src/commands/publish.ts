2bda65741d2403dedb743bb34b7d678cdd85a405
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-05-23 10:16:39 -0600

    feat: publish to graph network

---
+++ packages/cli/src/commands/publish.ts
@@ -0,0 +1,105 @@
+import chalk from 'chalk';
+import { print } from 'gluegun';
+import open from 'open';
+import { Args, Command, Flags, ux } from '@oclif/core';
+import { createCompiler } from '../command-helpers/compiler';
+import * as DataSourcesExtractor from '../command-helpers/data-sources';
+import { DEFAULT_IPFS_URL } from '../command-helpers/ipfs';
+import Protocol from '../protocols';
+
+export default class PublishCommand extends Command {
+  static description = 'Publish to the Graph Network';
+
+  static args = {
+    'subgraph-manifest': Args.string({
+      default: 'subgraph.yaml',
+    }),
+  };
+
+  static flags = {
+    help: Flags.help({
+      char: 'h',
+    }),
+    ipfs: Flags.string({
+      summary: 'Upload build results to an IPFS node.',
+      char: 'i',
+      default: DEFAULT_IPFS_URL,
+    }),
+    'ipfs-hash': Flags.string({
+      summary: 'IPFS hash of the subgraph manifest to deploy.',
+      required: false,
+    }),
+  };
+
+  /**
+   * Prompt the user to open up the browser to continue publishing the subgraph
+   */
+  async publishWithBrowser({ ipfsHash }: { ipfsHash: string }) {
+    const answer = await ux.prompt(
+      `Press ${chalk.green(
+        'y',
+      )} (or any key) to open up the browser to continue publishing or ${chalk.yellow(
+        'q',
+      )} to exit`,
+    );
+
+    if (answer.toLowerCase() === 'q') {
+      this.exit(0);
+    }
+
+    const URL = `https://cli.thegraph.com/deploy?id=${ipfsHash}`;
+
+    print.success(
+      `Finalize the publish of the subgraph from the Graph CLI publish page. Opening up the browser to continue publishing at ${URL}`,
+    );
+    await open(URL);
+    return;
+  }
+
+  async run() {
+    const {
+      args: { 'subgraph-manifest': manifest },
+      flags: {
+        'ipfs-hash': ipfsHash,
+
+        ipfs,
+      },
+    } = await this.parse(PublishCommand);
+
+    if (ipfsHash) {
+      await this.publishWithBrowser({ ipfsHash });
+      return;
+    }
+
+    let protocol;
+    try {
+      const dataSourcesAndTemplates = await DataSourcesExtractor.fromFilePath(manifest);
+      protocol = Protocol.fromDataSources(dataSourcesAndTemplates);
+    } catch (e) {
+      this.error(e, { exit: 1 });
+    }
+
+    const compiler = createCompiler(manifest, {
+      ipfs,
+      outputDir: 'build/',
+      outputFormat: 'wasm',
+      skipMigrations: false,
+      protocol,
+    });
+
+    // Exit with an error code if the compiler couldn't be created
+    if (!compiler) {
+      this.exit(1);
+      return;
+    }
+    const result = await compiler.compile({ validate: true });
+    if (result === undefined || result === false) {
+      // Compilation failed, not deploying.
+      process.exitCode = 1;
+      return;
+    }
+
+    await this.publishWithBrowser({ ipfsHash: result });
+    return;
+  }
+}
