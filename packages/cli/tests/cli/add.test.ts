8d9bbae6991179e16297c5ab51a56c3837544a61
Etienne Donneger <etienne@pinax.network>
2024-12-05 15:05:51 -0500

    Increase `hookTimeout` for Node18 tests

--- packages/cli/tests/cli/add.test.ts
+++ packages/cli/tests/cli/add.test.ts
@@ -8,43 +8,39 @@ const EXAMPLE_SUBGRAPH_PATH = path.join(__dirname, 'add', 'subgraph');
 
 const TEMP_SUBGRAPH_PATH = path.join(__dirname, 'add', 'tmp-subgraph');
 
-describe(
-  'Add command',
-  () => {
-    beforeAll(async () => {
-      toolbox.filesystem.remove(TEMP_SUBGRAPH_PATH); // If the tests fail before AfterAll is called
-      toolbox.filesystem.copy(EXAMPLE_SUBGRAPH_PATH, TEMP_SUBGRAPH_PATH);
+describe('Add command', () => {
+  beforeAll(async () => {
+    toolbox.filesystem.remove(TEMP_SUBGRAPH_PATH); // If the tests fail before AfterAll is called
+    toolbox.filesystem.copy(EXAMPLE_SUBGRAPH_PATH, TEMP_SUBGRAPH_PATH);
 
-      await runGraphCli(
-        [
-          'add',
-          '0x2E645469f354BB4F5c8a05B3b30A929361cf77eC',
-          '--contract-name',
-          'Gravatar',
-          '--abi',
-          `${EXAMPLE_SUBGRAPH_PATH}/abis/Gravity.json`,
-        ],
-        TEMP_SUBGRAPH_PATH,
-      );
-    });
+    await runGraphCli(
+      [
+        'add',
+        '0x2E645469f354BB4F5c8a05B3b30A929361cf77eC',
+        '--contract-name',
+        'Gravatar',
+        '--abi',
+        `${EXAMPLE_SUBGRAPH_PATH}/abis/Gravity.json`,
+      ],
+      TEMP_SUBGRAPH_PATH,
+    );
+  });
 
-    afterAll(async () => {
-      toolbox.filesystem.remove(TEMP_SUBGRAPH_PATH);
-    });
+  afterAll(async () => {
+    toolbox.filesystem.remove(TEMP_SUBGRAPH_PATH);
+  });
 
-    it('should add a new datasource to a subgraph', () => {
-      const manifest = fs.readFileSync(`${TEMP_SUBGRAPH_PATH}/subgraph.yaml`);
-      const expected = fs.readFileSync(`${__dirname}/add/expected/subgraph.yaml`);
+  it('should add a new datasource to a subgraph', () => {
+    const manifest = fs.readFileSync(`${TEMP_SUBGRAPH_PATH}/subgraph.yaml`);
+    const expected = fs.readFileSync(`${__dirname}/add/expected/subgraph.yaml`);
 
-      expect(manifest.equals(expected)).toBe(true);
-    });
+    expect(manifest.equals(expected)).toBe(true);
+  });
 
-    it('should not overwrite the ABI file', () => {
-      const abi = fs.readFileSync(`${TEMP_SUBGRAPH_PATH}/abis/Gravatar.json`);
-      const expected = fs.readFileSync(`${__dirname}/add/expected/Gravatar.json`);
+  it('should not overwrite the ABI file', () => {
+    const abi = fs.readFileSync(`${TEMP_SUBGRAPH_PATH}/abis/Gravatar.json`);
+    const expected = fs.readFileSync(`${__dirname}/add/expected/Gravatar.json`);
 
-      expect(expected.equals(abi)).toBe(true);
-    });
-  },
-  { timeout: 20_000 },
-);
+    expect(expected.equals(abi)).toBe(true);
+  });
+});
