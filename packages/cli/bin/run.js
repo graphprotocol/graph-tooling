71f1dd908556a85b9f0be2dfdffde4d00d81b5bd
YaroShkvorets <shkvorets@gmail.com>
2024-12-18 22:10:40 -0500

    error if node too old

--- packages/cli/bin/run.js
+++ packages/cli/bin/run.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
+import { readFileSync } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import semver from 'semver';
 import { execute } from '@oclif/core';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pkg = JSON.parse(readFileSync(join(__dirname, '..', 'package.json'), 'utf8'));
+
+const requiredVersion = pkg.engines.node;
+const currentVersion = process.version;
+
+if (!semver.satisfies(currentVersion, requiredVersion)) {
+  console.error(
+    `Error: Node.js version ${requiredVersion} is required. Current version: ${currentVersion}`,
+  );
+  process.exit(1);
+}
 
 await execute({ dir: import.meta.url });
