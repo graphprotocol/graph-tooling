b4564a71134c310dee983f6eff613b953bf481be
Yaro Shkvorets <shkvorets@gmail.com>
2025-01-07 19:43:57 -0500

    Add typecasting for tuple[] and address[] (#1892)
    * wrap address[] and tuple[] into changetype
    
    * tests
    
    * pass imports array
    
    * cleanup mapping codegen
    
    * changeset
    
    * add issue to changeset

--- packages/cli/src/scaffold/mapping.ts
+++ packages/cli/src/scaffold/mapping.ts
@@ -1,17 +1,58 @@
 import * as util from '../codegen/util.js';
 
-export const generateFieldAssignment = (key: string[], value: string[]) =>
-  `entity.${key.join('_')} = event.params.${value.join('.')}`;
+/**
+ * Map of value types that need to be changetype'd to their corresponding AssemblyScript type
+ */
+export const VALUE_TYPECAST_MAP: Record<string, string> = {
+  'address[]': 'Bytes[]',
+  'tuple[]': 'Bytes[]',
+};
 
-export const generateFieldAssignments = ({ index, input }: { index: number; input: any }) =>
-  input.type === 'tuple'
-    ? util
-        .unrollTuple({ value: input, index, path: [input.name || `param${index}`] })
-        .map(({ path }: any) => generateFieldAssignment(path, path))
-    : generateFieldAssignment(
-        [(input.mappedName ?? input.name) || `param${index}`],
-        [input.name || `param${index}`],
-      );
+export const generateFieldAssignment = (
+  key: string[],
+  value: string[],
+  type: string,
+): { assignment: string; imports: string[] } => {
+  let rightSide = `event.params.${value.join('.')}`;
+  const imports = [];
+
+  if (type in VALUE_TYPECAST_MAP) {
+    const castTo = VALUE_TYPECAST_MAP[type];
+    rightSide = `changetype<${castTo}>(${rightSide})`;
+    imports.push(castTo.replace('[]', ''));
+  }
+
+  return {
+    assignment: `entity.${key.join('_')} = ${rightSide}`,
+    imports,
+  };
+};
+
+export const generateFieldAssignments = ({
+  index,
+  input,
+}: {
+  index: number;
+  input: any;
+}): { assignments: string[]; imports: string[] } => {
+  const fields =
+    input.type === 'tuple'
+      ? util
+          .unrollTuple({ value: input, index, path: [input.name || `param${index}`] })
+          .map(({ path, type }) => generateFieldAssignment(path, path, type))
+      : [
+          generateFieldAssignment(
+            [(input.mappedName ?? input.name) || `param${index}`],
+            [input.name || `param${index}`],
+            input.type,
+          ),
+        ];
+
+  return {
+    assignments: fields.map(a => a.assignment),
+    imports: fields.map(a => a.imports).flat(),
+  };
+};
 
 /**
  * Map of input names that are reserved so we do not use them as field names to avoid conflicts
@@ -27,26 +68,34 @@ export const renameNameIfNeeded = (name: string) => {
   return NAMES_REMAP_DICTIONARY[name] ?? name;
 };
 
-export const generateEventFieldAssignments = (event: any, _contractName: string) =>
-  event.inputs.reduce((acc: any[], input: any, index: number) => {
-    input.mappedName = renameNameIfNeeded(input.name);
-    return acc.concat(generateFieldAssignments({ input, index }));
-  }, []);
+export const generateEventFieldAssignments = (
+  event: any,
+  _contractName: string,
+): { assignments: string[]; imports: string[] } =>
+  event.inputs.reduce(
+    (acc: any, input: any, index: number) => {
+      input.mappedName = renameNameIfNeeded(input.name);
+      const { assignments, imports } = generateFieldAssignments({ input, index });
+      return {
+        assignments: acc.assignments.concat(assignments),
+        imports: acc.imports.concat(imports),
+      };
+    },
+    { assignments: [], imports: [] },
+  );
 
-export const generateEventIndexingHandlers = (events: any[], contractName: string) =>
-  `
-  import { ${events.map(
-    event => `${event._alias} as ${event._alias}Event`,
-  )}} from '../generated/${contractName}/${contractName}'
-  import { ${events.map(event => event._alias)} } from '../generated/schema'
+export const generateEventIndexingHandlers = (events: any[], contractName: string) => {
+  const eventFieldAssignments = events.map(event => ({
+    event,
+    ...generateEventFieldAssignments(event, contractName),
+  }));
+  const allImports = [...new Set(eventFieldAssignments.map(({ imports }) => imports).flat())];
 
-  ${events
-    .map(
-      event =>
-        `
+  const eventHandlers = eventFieldAssignments.map(({ event, assignments }) => {
+    return `
   export function handle${event._alias}(event: ${event._alias}Event): void {
     let entity = new ${event._alias}(event.transaction.hash.concatI32(event.logIndex.toI32()))
-    ${generateEventFieldAssignments(event, contractName).join('\n')}
+    ${assignments.join('\n')}
 
     entity.blockNumber = event.block.number
     entity.blockTimestamp = event.block.timestamp
@@ -54,7 +103,20 @@ export const generateEventIndexingHandlers = (events: any[], contractName: strin
 
     entity.save()
   }
-    `,
-    )
-    .join('\n')}
+    `;
+  });
+
+  return `
+  import { ${events.map(
+    event => `${event._alias} as ${event._alias}Event`,
+  )}} from '../generated/${contractName}/${contractName}'
+  import { ${events.map(event => event._alias)} } from '../generated/schema'
+  ${
+    allImports.length > 0
+      ? `import { ${allImports.join(', ')} } from '@graphprotocol/graph-ts'`
+      : ''
+  }
+
+  ${eventHandlers.join('\n')}
 `;
+};
