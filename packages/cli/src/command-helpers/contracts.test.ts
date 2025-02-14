0c34c86e497a4a0429b3441f6ec1dc57a58cd487
YaroShkvorets <shkvorets@gmail.com>
2025-02-14 09:37:09 -0500

    chore: fix lint

--- packages/cli/src/command-helpers/contracts.test.ts
+++ packages/cli/src/command-helpers/contracts.test.ts
@@ -145,9 +145,9 @@ async function retry<T>(operation: () => Promise<T>, maxRetries = 3, sleepMs = 5
 const withTimeout = <T>(promise: Promise<T>, timeoutMs: number): Promise<T> => {
   return Promise.race([
     promise,
-    new Promise<T>((_, reject) => 
-      setTimeout(() => reject(new Error(`Operation timed out after ${timeoutMs}ms`)), timeoutMs)
-    )
+    new Promise<T>((_, reject) =>
+      setTimeout(() => reject(new Error(`Operation timed out after ${timeoutMs}ms`)), timeoutMs),
+    ),
   ]);
 };
 
